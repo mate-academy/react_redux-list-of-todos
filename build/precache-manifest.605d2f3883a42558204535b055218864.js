ad0a9cd6f40cb922472b08d46bcdf1c6ae4ee837
Onisko <Onisko@MacBook.local>
2019-07-07 12:02:11 +0300

    build

---
+++ build/precache-manifest.605d2f3883a42558204535b055218864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b86e655878b32e3808bf7481fda325f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f5c6c746e8211ffa036",
+    "url": "./static/css/main.9e5f5e38.chunk.css"
+  },
+  {
+    "revision": "d05f2bff1f5b67197faa",
+    "url": "./static/js/2.c90744ad.chunk.js"
+  },
+  {
+    "revision": "1f5c6c746e8211ffa036",
+    "url": "./static/js/main.ec410d9f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
