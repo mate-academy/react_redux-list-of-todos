60ec606304b39b75bbc4939443e78f3a21a90722
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-10-11 07:45:05 +0300

    Solution

---
+++ build/precache-manifest.b507569375fe619266702d6039af0883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16061a778d0fdf4e9eaf08871af76a8e",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "1e987d733e479de2ebc1",
+    "url": "/react_redux-list-of-todos/static/css/main.57d4e02f.chunk.css"
+  },
+  {
+    "revision": "c74170ce5909df0b904a",
+    "url": "/react_redux-list-of-todos/static/js/2.c353344b.chunk.js"
+  },
+  {
+    "revision": "1e987d733e479de2ebc1",
+    "url": "/react_redux-list-of-todos/static/js/main.38e763e4.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
