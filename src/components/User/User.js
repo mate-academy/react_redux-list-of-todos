b977d3af5e57e0ef7f4a6213cf4fb94819a14cd3
SakVladyslav <iblackvour@gmail.com>
2019-10-15 21:17:04 +0300

    Start a coding

---
+++ src/components/User/User.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './User.css';
+
+const User = ({ user }) => {
+  const {
+    username, email, name, phone, website,
+  } = user;
+
+  return (
+    <div className="user">
+      <p className="user__name">{name}</p>
+      <p className="user__username">{username}</p>
+      <div className="user__details">
+        <span className="user__phone">{phone}</span>
+        <a href=":mail" className="user__email">{email}</a>
+        <a href="#1" className="user__website">{website}</a>
+      </div>
+    </div>
+  );
+};
+
+export default User;
