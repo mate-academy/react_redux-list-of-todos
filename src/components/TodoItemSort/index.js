afc665b58ce4a1f44766868991b72a6d3a7e9299
KosBelozyorov <KosBelozyorov@gmail.com>
2019-10-24 12:10:18 +0300

    add mapDispatchToProps shorthand

--- src/components/TodoItemSort/index.js
+++ src/components/TodoItemSort/index.js
@@ -2,13 +2,9 @@ import { connect } from 'react-redux';
 import TodoItemSort from './TodoItemSort';
 import { sortData } from '../../store';
 
-const mapDispatchToProps = {
-  sortData: sortData,
-};
-
 const EnhancedTodoItemSort = connect(
   ({ sortMethod }) => ({ sortMethod }),
-  mapDispatchToProps
+  { sortData }
 )(TodoItemSort);
 
 export {
