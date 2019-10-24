25c83ee958828c048fb6507e2031452099ad9097
KosBelozyorov <KosBelozyorov@gmail.com>
2019-10-24 11:39:13 +0300

    update fix Solution

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,10 +2,6 @@ import { connect } from 'react-redux';
 import TodoList from './TodoList';
 import { sortTodos } from '../../store';
 
-// const mapDispatchToProps = dispatch => ({
-//   sortTodos: () => dispatch(sortTodos()),
-// });
-
 const EnhancedTodoList = connect(
   state => ({
     todos: state.todos,
