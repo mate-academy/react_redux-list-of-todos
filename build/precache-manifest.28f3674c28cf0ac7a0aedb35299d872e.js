3d7fa1e03fd7f631817b9db91c7b9b7dd79bafb0
boom40 <kickgames675@gmail.com>
2020-01-10 01:00:47 +0200

    solution

---
+++ build/precache-manifest.28f3674c28cf0ac7a0aedb35299d872e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60ba5620bf47102e91ec0becac50f33",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "19d5be9fe345f42b0c96",
+    "url": "/react_redux-list-of-todos/static/css/main.85ae2195.chunk.css"
+  },
+  {
+    "revision": "93e332c6af172a38a9c3",
+    "url": "/react_redux-list-of-todos/static/js/2.4f5b4145.chunk.js"
+  },
+  {
+    "revision": "19d5be9fe345f42b0c96",
+    "url": "/react_redux-list-of-todos/static/js/main.ecaabb68.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
