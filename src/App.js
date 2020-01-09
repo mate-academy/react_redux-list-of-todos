bdaf3795e79d7ea1c3e14a6d01eb678c6e4444b5
Vivych Oksana <vivichokcana@gmail.com>
2020-01-09 23:29:17 +0200

    'Solution'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  loadDataFromServer,
+  getIsLoading,
+  getIsError,
+  getCombinedTodos,
+} from './Components/List';
+import TodoList from './Components/TodoList';
 
-function App() {
+const App = ({ combinedTodos, isLoading, isError, loadData }) => {
+  const startDataLoading = async() => {
+    await loadData();
+  };
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <h1>Redux list of todos</h1>
+      {combinedTodos.length ? (
+        <TodoList list={combinedTodos} />
+      ) : (
+        <>
+          <h2>{isError ? 'Error' : ''}</h2>
+          <button type="button" onClick={startDataLoading}>
+            {isLoading ? 'Loading...' : 'Load'}
+          </button>
+        </>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+const getData = state => ({
+  combinedTodos: getCombinedTodos(state),
+  isLoading: getIsLoading(state),
+  isError: getIsError(state),
+});
+
+const getMethods = {
+  loadData: loadDataFromServer,
+};
+
+App.propTypes = {
+  combinedTodos: PropTypes.arrayOf(
+    PropTypes.object
+  ),
+  isLoading: PropTypes.bool,
+  isError: PropTypes.bool,
+  loadData: PropTypes.func,
+}.isRequired;
+
+export default connect(getData, getMethods)(App);
