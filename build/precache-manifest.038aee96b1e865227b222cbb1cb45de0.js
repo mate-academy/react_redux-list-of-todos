f51187493da53a35c92169fca8d9479a6c5f8ab9
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-01-09 22:59:24 +0200

    solution

---
+++ build/precache-manifest.038aee96b1e865227b222cbb1cb45de0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a56ea73071e08063dd93230ec967dab",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "4b0ef5d62b3f7d61e7e9",
+    "url": "/react_redux-list-of-todos/static/css/main.1e46429e.chunk.css"
+  },
+  {
+    "revision": "0efb760fa4eb42d75385",
+    "url": "/react_redux-list-of-todos/static/js/2.dfe72ccd.chunk.js"
+  },
+  {
+    "revision": "4b0ef5d62b3f7d61e7e9",
+    "url": "/react_redux-list-of-todos/static/js/main.87b31173.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
