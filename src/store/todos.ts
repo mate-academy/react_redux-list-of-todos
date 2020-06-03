2b3d47e76bf1bd6c93031c20974cf968b7fd4ba0
euxos <eugen.ostrozhinsky@gmail.com>
2020-06-03 15:12:38 +0300

    delete btn was added

--- src/store/todos.ts
+++ src/store/todos.ts
@@ -1,15 +1,29 @@
 import { Action } from 'redux';
 
-const TODOS = 'TODOS';
+const LOAD_TODOS = 'LOAD_TODOS';
+const DELETE_TODO = 'DELETE_TODO';
 
-type Todos = Action<typeof TODOS> & { todos: Todo[] };
+type LoadTodos = Action<typeof LOAD_TODOS> & { todos: Todo[] };
+type DeleteTodo = Action<typeof DELETE_TODO> & { id: number };
 
-export const setTodosAction = (todos: Todo[]): Todos => ({ type: TODOS, todos });
+export const setTodosAction = (todos: Todo[]): LoadTodos => ({
+  type: LOAD_TODOS,
+  todos,
+});
+export const deleteTodoAction = (id: number): DeleteTodo=> ({
+  type: DELETE_TODO,
+  id,
+});
+
+type Todos = LoadTodos | DeleteTodo;
 
 const todosReducer = (state = [] as Todo[], action: Todos) => {
   switch (action.type) {
-    case TODOS:
+    case LOAD_TODOS:
       return action.todos;
+
+    case DELETE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
 
     default:
       return state;
