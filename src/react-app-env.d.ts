f235d5462808361889eb04a94c948e8eb18c9a0e
Illia Oliynik <illiaoliynik@gmail.com>
2020-06-04 08:26:19 +0300

    solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,14 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+interface User {
+  username: string;
+  id: number;
+}
+
+interface Todo {
+  title: string;
+  id: number;
+  userId: number;
+  user?: User;
+  completed: boolean;
+}
