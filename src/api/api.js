9f588cfc827a9989d2572459a2342ef6652f1bd3
AntonChetveryk <chetverykanton92@gmail.com>
2020-11-26 12:54:29 +0200

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://mate-api.herokuapp.com/";
+
+export const request = (url, options) => {
+  return fetch(`${BASE_URL}${url}`, options).then((res) => {
+    if (!res.ok) {
+      throw new Error(`${res.status}`);
+    }
+
+    return res.json();
+  });
+};
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,26 @@
-import { createStore, AnyAction } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, AnyAction, applyMiddleware, Dispatch } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { request } from "../api/api";
+import thunk from "redux-thunk";
 
 // Action types - is just a constant. MUST have a unique value.
-const START_LOADING = 'START_LOADING';
-const FINISH_LOADING = 'FINISH_LOADING';
+const START_LOADING = "START_LOADING";
+const FINISH_LOADING = "FINISH_LOADING";
+const GET_TODOS = "GET_TODOS";
 
 // Action creators - a function returning an action object
 export const startLoading = () => ({ type: START_LOADING });
-export const finishLoading = (message = 'No message') => ({ type: FINISH_LOADING, message });
+export const finishLoading = (message = "No message") => ({
+  type: FINISH_LOADING,
+  message,
+});
+export const getTodos = (todos: any[]) => ({
+  type: GET_TODOS,
+  todos,
+});
+export const fetchTodos = () => (dispatch: Dispatch) => {
+  request("todos").then((res) => dispatch(getTodos(res.data)));
+};
 
 // Selectors - a function receiving Redux state and returning some data from it
 export const isLoading = (state: RootState) => state.loading;
@@ -17,11 +30,13 @@ export const getMessage = (state: RootState) => state.message;
 export type RootState = {
   loading: boolean;
   message: string;
+  todos: any[];
 };
 
 const initialState: RootState = {
   loading: false,
-  message: '',
+  message: "",
+  todos: [],
 };
 
 // rootReducer - this function is called after dispatching an action
@@ -37,6 +52,13 @@ const rootReducer = (state = initialState, action: AnyAction) => {
         message: action.message,
       };
 
+    case GET_TODOS:
+      console.log("todos");
+      return {
+        ...state,
+        todos: action.todos,
+      };
+
     default:
       return state;
   }
@@ -45,7 +67,7 @@ const rootReducer = (state = initialState, action: AnyAction) => {
 // The `store` should be passed to the <Provider store={store}> in `/src/index.tsx`
 const store = createStore(
   rootReducer,
-  composeWithDevTools(), // allows you to use http://extension.remotedev.io/
+  composeWithDevTools(applyMiddleware(thunk)) // allows you to use http://extension.remotedev.io/
 );
 
 export default store;
