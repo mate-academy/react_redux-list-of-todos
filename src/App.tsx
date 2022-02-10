91f9e100cc238e4d416b0ce831edfac01672b1dd
DenysCheporniuk <denys.cheporniuk@gmail.com>
2022-02-10 18:37:41 +0200

    react redux todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-
 import './App.scss';
-import Start from './components/Start';
-import { Finish } from './components/Finish';
-
-import { isLoading, getMessage } from './store';
-
+import { TodosList } from './components/TodosList';
+import { CurrentUser } from './components/CurrentUser';
 
 const App = () => {
-  const loading = useSelector(isLoading);
-  const message = useSelector(getMessage) || 'Ready!';
+  // eslint-disable-next-line no-console
+  console.log('app render');
 
   return (
     <div className="App">
-      <h1>Redux list of todos</h1>
-      <h2>{loading ? 'Loading...' : message}</h2>
-
-      <Start title="Start loading" />
-      <Finish title="Succeed loading" message="Loaded successfully!" />
-      <Finish title="Fail loading" message="An error occurred when loading data." />
+      <TodosList />
+      <CurrentUser />
     </div>
   );
 };
