7b76418257ac3fd2f8362257cd5f89c1b41bf72b
Valeria Valuyskaya <valeriavaluyskaya@gmail.com>
2022-02-11 15:50:00 +0200

    Solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,28 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+type Todo = {
+  id: number,
+  userId: number,
+  title: string,
+  createdAt: string,
+  updatedAt: string,
+  completed: boolean,
+};
+
+type User = {
+  id: number,
+  name: string,
+  email: string,
+  phone: string,
+};
+
+export type State = {
+  todos: Todo[]
+  user: User | null
+};
+
+export type Action = {
+  type: string,
+  payload: any,
+};
