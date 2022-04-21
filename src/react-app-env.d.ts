f4f1e3d6308f2b879779a5b7a1e7b6507c6991d7
Max Shlikhta <maxok.vibe@gmail.com>
2022-04-21 21:58:21 +0300

    add all files

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,16 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
