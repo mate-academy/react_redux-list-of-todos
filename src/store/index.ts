4b5daf4df6a668e58cd2fcdd036eafbe641bed24
Vladyslav Kurochka <vlados123sos@gmail.com>
2022-06-02 20:32:53 +0300

    add components

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,42 +2,86 @@ import { createStore, AnyAction } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Action types - is just a constant. MUST have a unique value.
-const START_LOADING = 'START_LOADING';
-const FINISH_LOADING = 'FINISH_LOADING';
+const LOADING_TODOS = 'LOADING_TODOS';
+const SELECT_USER = 'SELECT_USER';
+const GET_ERROR = 'GET_ERROR';
+const GET_USER = 'GET_USER';
+const DELETE_TODO = 'DELETE_TODO';
 
 // Action creators - a function returning an action object
-export const startLoading = () => ({ type: START_LOADING });
-export const finishLoading = (message = 'No message') => ({
-  type: FINISH_LOADING,
-  message,
-});
+export const actions = {
+  loading__todos: (todos: Todo[]) => ({
+    type: LOADING_TODOS,
+    todos,
+  }),
+  selectUser: (userId: number) => ({
+    type: SELECT_USER,
+    userId,
+  }),
+  getError: (message: string) => ({
+    type: GET_ERROR,
+    message,
+  }),
+  getUser: (user: User) => ({
+    type: GET_USER,
+    user,
+  }),
+  deleteTodo: (id: number) => ({
+    type: DELETE_TODO,
+    id,
+  }),
+};
 
 // Selectors - a function receiving Redux state and returning some data from it
-export const isLoading = (state: RootState) => state.loading;
-export const getMessage = (state: RootState) => state.message;
+export const selectors = {
+  loadTodos: (state: RootState) => state.todos,
+  getUserId: (state: RootState) => state.userId,
+  getError: (state: RootState) => state.errorLoading,
+  loadUser: (state: RootState) => state.user,
+};
 
 // Initial state
 export type RootState = {
-  loading: boolean;
-  message: string;
+  todos: Todo[];
+  userId: number;
+  errorLoading: string;
+  user: User | null,
+  sort: boolean,
 };
 
 const initialState: RootState = {
-  loading: false,
-  message: '',
+  todos: [],
+  userId: 0,
+  errorLoading: '',
+  user: null,
+  sort: false,
 };
 
 // rootReducer - this function is called after dispatching an action
 const rootReducer = (state = initialState, action: AnyAction) => {
   switch (action.type) {
-    case START_LOADING:
-      return { ...state, loading: true };
+    case LOADING_TODOS:
+      return { ...state, todos: action.todos };
 
-    case FINISH_LOADING:
+    case SELECT_USER:
       return {
         ...state,
-        loading: false,
+        userId: action.userId,
+      };
+    case GET_ERROR:
+      return {
+        ...state,
         message: action.message,
+      };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.id),
       };
 
     default:
