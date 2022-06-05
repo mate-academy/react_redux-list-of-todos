5f42b458180ddc71c175a488250511472dbaf1e6
Alexandr Nikytenko <nika200382@gmail.com>
2022-06-05 20:57:46 +0300

    solution

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,14 @@
-// eslint-disable-next-line
-/// <reference types="react-scripts" />
+export type Todo = {
+  id: number,
+  userId: number,
+  title: string,
+  completed: boolean,
+};
+
+export type User = {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  phone: number,
+};
