de74edd760346656b7b49bd05709a44899f6c8da
Daria Liubymova <dellachaise@gmail.com>
2022-06-27 15:57:24 +0300

    Implemented 'Redux list of todos' task

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import { useSelector } from 'react-redux';
-
+import React from 'react';
 import './App.scss';
-import Start from './components/Start';
-import { Finish } from './components/Finish';
+import './styles/general.scss';
+import { TodoList } from './components/TodoList/TodoList';
+import { CurrentUser } from './components/CurrentUser/CurrentUser';
 
-import { isLoading, getMessage } from './store';
-
-const App = () => {
-  const loading = useSelector(isLoading);
-  const message = useSelector(getMessage) || 'Ready!';
-
-  return (
-    <div className="App">
-      <h1>Redux list of todos</h1>
-      <h2>{loading ? 'Loading...' : message}</h2>
-
-      <Start title="Start loading" />
-      <Finish title="Succeed loading" message="Loaded successfully!" />
-      <Finish
-        title="Fail loading"
-        message="An error occurred when loading data."
-      />
+const App: React.FC = () => (
+  <div className="App">
+    <div className="App__sidebar">
+      <TodoList />
     </div>
-  );
-};
+
+    <div className="App__content">
+      <div className="App__content-container">
+        <CurrentUser />
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,36 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+interface Todo {
+  id: number,
+  createdAt: string,
+  updatedAt: string,
+  userId: number,
+  title: string,
+  completed: boolean,
+}
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  phone: string,
+  website: string,
+  createdAt: string,
+  updatedAt: string,
+  address: {
+    id: number,
+    userId: number,
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    createdAt: string,
+    updatedAt: string
+  }
+}
+
+export interface State {
+  todos: Todo[],
+  user: User | null,
+}
