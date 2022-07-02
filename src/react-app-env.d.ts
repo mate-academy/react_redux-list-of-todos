9e642ebfa2874091dfa0e845e28baa68688d54cf
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-07-02 23:18:52 +0300

    Redux-todos

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,31 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+export interface Todo {
+  id: number,
+  userId: number,
+  title: string,
+  completed: boolean,
+}
+
+export interface State {
+  todos: Todo[],
+  user: User | null,
+}
+
+export interface Action {
+  type: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  payLoad: any,
+}
+
+export interface User {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+}
