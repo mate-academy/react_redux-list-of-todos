2dd745a89eb6b1850b2d080a2e02a607f4865f9f
Vicky Miziukanova <enrikadej@gmail.com>
2022-07-04 17:00:08 +0300

    task without toolkit and debouce

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,23 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+export interface Todo {
+  id:number,
+  userId:number,
+  title:string,
+  completed:false,
+}
+
+export interface State {
+  todos: Todo[],
+  user: User | null,
+}
+
+export interface User {
+  id:number,
+  name:string,
+  username:string,
+  email:string,
+  phone:string,
+  website:string
+}
