25adbb0b7523105ed64673490745f364d2e87634
Olha Stakhova <olha.stakhova@gmail.com>
2022-09-07 01:20:36 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,75 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { useDispatch, useSelector } from 'react-redux';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { TODO_ACTIONS } from './features/todos';
+import { Todo } from './types/Todo';
+import { FILTER_SELECTOR, CURRENT_TODO_SELECT, TODOS_SELECTOR } from './features/selectors';
+
+const filtredByComleted = (nameFilter: string, todosList: Todo[]): Todo[] => {
+  switch (nameFilter) {
+    case 'completed':
+      return todosList.filter(todo => todo.completed === true);
+
+    case 'active':
+      return todosList.filter(todo => todo.completed === false);
+
+    case 'all':
+      return todosList;
+
+    default:
+      return todosList;
+  }
+};
+
+const filredByQuery = (searchQury: string, todosList: Todo[]): Todo[] => {
+  if (searchQury === '') {
+    return todosList;
+  }
+
+  return (todosList
+    .filter(
+      todo => todo.title.toLowerCase().includes(searchQury.toLowerCase()),
+    ));
+};
 
 export const App: React.FC = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const dispach = useDispatch();
+
+  const todos = useSelector(TODOS_SELECTOR.todos);
+
+  const selectTodo = useSelector(CURRENT_TODO_SELECT.currentTodo);
+
+  const [filteredArray, setFilteredArray] = useState([]);
+
+  const { status, query } = useSelector(FILTER_SELECTOR.filter);
+
+  useEffect(() => {
+    const filteredBySelect = filtredByComleted(status, todos);
+
+    // setFilteredArray(() => filredByQuery(query, filteredBySelect));
+    setFilteredArray((): any => filredByQuery(query, filteredBySelect));
+  }, [status, query, todos]);
+
+  useEffect(() => {
+    setIsLoaded(false);
+    getTodos()
+      .then(todosData => {
+        if (todosData && todosData.length) {
+          dispach(TODO_ACTIONS.set(todosData));
+          setIsLoaded(true);
+        }
+      });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +82,17 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!isLoaded
+                ? <Loader />
+                : (
+                  <TodoList filteredTodos={filteredArray} />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectTodo && <TodoModal selectTodo={selectTodo} /> }
     </>
   );
 };
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,45 @@
+import { Action as ActionBase } from 'redux';
+// import { RootState } from '../app/store';
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+interface Action<T, P> extends ActionBase<T> {
+  payload: P,
+}
 
-const todosReducer = (): Todo[] => {
-  return [];
+export enum TodosActionsTypes {
+  SetTodo = 'todos/set_todos',
+}
+
+export type SetTodosAction = Action<TodosActionsTypes.SetTodo, Todo[]>;
+
+const setTodosActionCreator = (todos: Todo[]): SetTodosAction => ({
+  type: TodosActionsTypes.SetTodo,
+  payload: todos,
+});
+
+export const TODO_ACTIONS = {
+  set: setTodosActionCreator,
 };
+
+type TodosActions = SetTodosAction;
+
+export const todosReducer = (
+  todosState: Todo[] = [],
+  actions: TodosActions,
+): Todo[] => {
+  switch (actions.type) {
+    case TodosActionsTypes.SetTodo:
+      return actions.payload;
+
+    default:
+      return todosState;
+  }
+};
+
+// const todosSelector = (state: RootState) => state.todos;
+
+// export const TODOS_SELECTOR = {
+//   todos: todosSelector,
+// };
 
 export default todosReducer;
