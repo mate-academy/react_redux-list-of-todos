250c11a67b0f9917d59ad8abb2e3e364f5e9f5cb
Leoned Pustovit <lrerednawl@gmail.com>
2022-09-07 16:10:18 +0300

    Solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,62 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+// #region Types
+type SetQuery = {
+  type: 'filter/SETQUERY',
+  payload: string,
+};
+type SetStatus = {
+  type: 'filter/SETSTATUS',
+  payload: Status,
+};
+type ResetState = { type: 'filter/RESET' };
+type ResetQuery = { type: 'filter/RESETQUERY' };
+// #endregion
+
+// #region Actions
+const setQuery = (text: string): SetQuery => (
+  { type: 'filter/SETQUERY', payload: text }
+);
+const setStatus = (newStatus: Status): SetStatus => (
+  { type: 'filter/SETSTATUS', payload: newStatus }
+);
+const resetState = (): ResetState => ({ type: 'filter/RESET' });
+const resetQuery = (): ResetQuery => ({ type: 'filter/RESETQUERY' });
+
+export const actions = {
+  setQuery,
+  setStatus,
+  resetState,
+  resetQuery,
+};
+// #endregion
+
+export type FilterState = {
+  query: string,
+  status: Status,
+};
+type Action = SetQuery | SetStatus | ResetState | ResetQuery;
+
+const filterReducer = (
+  state: FilterState = { query: '', status: 'all' },
+  action: Action,
+): FilterState => {
+  switch (action.type) {
+    case 'filter/SETQUERY':
+      return { ...state, query: action.payload };
+
+    case 'filter/SETSTATUS':
+      return { ...state, status: action.payload };
+
+    case 'filter/RESET':
+      return { query: '', status: 'all' };
+
+    case 'filter/RESETQUERY':
+      return { ...state, query: '' };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
