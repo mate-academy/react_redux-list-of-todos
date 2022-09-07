0d1bd9e1b28da1a14dc33a4d2ed442db76476d4c
Vitaliy <vitmixgt@icloud.com>
2022-09-07 09:42:54 +0300

    import old project

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,41 +1,60 @@
-import React from 'react';
+import { FC } from 'react';
+import { FilterBy } from '../../types/Todo';
 
-export const TodoFilter: React.FC = () => {
-  return (
-    <form
-      className="field has-addons"
-      onSubmit={event => event.preventDefault()}
-    >
-      <p className="control">
-        <span className="select">
-          <select data-cy="statusSelect">
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
-          </select>
-        </span>
-      </p>
+interface Props {
+  searchQuery: string,
+  setSearchQuery: (title:string) => void,
+  completedFilter: string,
+  setCompletedFilter: (filter: FilterBy) => void,
+}
 
-      <p className="control is-expanded has-icons-left has-icons-right">
-        <input
-          data-cy="searchInput"
-          type="text"
-          className="input"
-          placeholder="Search..."
-        />
-        <span className="icon is-left">
-          <i className="fas fa-magnifying-glass" />
-        </span>
+export const TodoFilter: FC<Props> = ({
+  searchQuery,
+  setSearchQuery,
+  completedFilter,
+  setCompletedFilter,
+}) => (
+  <form className="field has-addons">
+    <p className="control">
+      <span className="select">
+        <select
+          data-cy="statusSelect"
+          value={completedFilter}
+          onChange={event => setCompletedFilter(event.target.value as FilterBy)}
+        >
+          <option value={FilterBy.NONE}>All</option>
+          <option value={FilterBy.ACTIVE}>Active</option>
+          <option value={FilterBy.COMPLETED}>Completed</option>
+        </select>
+      </span>
+    </p>
 
+    <p className="control is-expanded has-icons-left has-icons-right">
+      <input
+        data-cy="searchInput"
+        type="text"
+        className="input"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(event) => (
+          setSearchQuery(event.target.value)
+        )}
+      />
+      <span className="icon is-left">
+        <i className="fas fa-magnifying-glass" />
+      </span>
+
+      {searchQuery.length > 0 && (
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
           <button
             data-cy="clearSearchButton"
             type="button"
             className="delete"
+            onClick={() => setSearchQuery('')}
           />
         </span>
-      </p>
-    </form>
-  );
-};
+      )}
+    </p>
+  </form>
+);
