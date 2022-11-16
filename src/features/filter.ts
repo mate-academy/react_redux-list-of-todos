898449ca856af59375377acd3d79e9a7140f5490
Artem Kodesnikov <artiguard@gmail.com>
2022-11-16 18:22:49 +0200

    redux list of todos

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,41 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type StatusAction = { type: 'filters/STATUS', payload: Status };
+type QueryAction = { type: 'filters/QUERY', payload: string };
+
+type Action = StatusAction | QueryAction;
+
+const status = (value: Status): StatusAction => ({
+  type: 'filters/STATUS',
+  payload: value,
+});
+
+const query = (value: string): QueryAction => ({
+  type: 'filters/QUERY',
+  payload: value,
+});
+
+const initialState = {
+  query: '',
+  status: 'all',
 };
+
+const filterReducer = (
+  filters = initialState,
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'filters/STATUS':
+      return { ...filters, status: action.payload };
+
+    case 'filters/QUERY':
+      return { ...filters, query: action.payload };
+
+    default:
+      return filters;
+  }
+};
+
+export const actions = { status, query };
 
 export default filterReducer;
