6b3175a695cd95fe823b061fcb48ebe03932d327
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-11-22 17:52:05 +0200

    solution

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,56 @@
+import classNames from 'classnames';
+import { Todo } from '../../types/Todo';
+import { actions as todoActions } from '../../features/currentTodo';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoItem: React.FC<Props> = ({ todo }) => {
+  const { id, title, completed } = todo;
+  const dispatch = useAppDispatch();
+  const currentTodoId = useAppSelector((state) => state.currentTodo?.id);
+
+  return (
+    <tr data-cy="todo" key={id}>
+      <td className="is-vcentered">{id}</td>
+      <td className="is-vcentered">
+        {completed && (
+          <span className="icon" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        )}
+      </td>
+
+      <td className="is-vcentered is-expanded">
+        <p
+          className={classNames({
+            'has-text-success': completed,
+            'has-text-danger': !completed,
+          })}
+        >
+          {title}
+        </p>
+      </td>
+
+      <td className="has-text-right is-vcentered">
+        <button
+          data-cy="selectButton"
+          className="button"
+          type="button"
+          onClick={() => dispatch(todoActions.setTodo(todo))}
+        >
+          <span className="icon">
+            <i
+              className={classNames('far', {
+                'fa-eye': currentTodoId !== id,
+                'fa-eye-slash': currentTodoId === id,
+              })}
+            />
+          </span>
+        </button>
+      </td>
+    </tr>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,40 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { Loader } from '../Loader';
+import { actions as todoActions } from '../../features/currentTodo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
 export const TodoModal: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+
+    const data = async () => {
+      try {
+        if (!currentTodo) {
+          return;
+        }
+
+        const visibleUser = await getUser(currentTodo.userId);
+
+        setUser(visibleUser);
+      } catch (error) {
+        throw new Error('error');
+      } finally {
+        setIsLoaded(false);
+      }
+    };
+
+    data();
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {isLoaded && <Loader />}
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+      {user && (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${currentTodo?.id}`}
+            </div>
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              aria-label="button"
+              onClick={() => dispatch(todoActions.deleteTodo())}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {currentTodo?.title}
+            </p>
+            <p className="block" data-cy="modal-user">
+              {currentTodo?.completed
+                ? (<strong className="has-text-success">Done</strong>)
+                : (<strong className="has-text-danger">Planned</strong>)}
+              {' by '}
+              <a href={`mailto:${user?.email}`}>
+                {user?.name}
+              </a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
         </div>
-      </div>
+
+      )}
     </div>
   );
 };
