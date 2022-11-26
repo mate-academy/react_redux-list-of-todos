e15cdf273411e55dc145fbf1af0f06571f03b285
Dima <dima3679@MacBook-Air-Marina.local>
2022-11-26 22:36:00 +0200

    done

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,26 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type GetTodos = { type: 'todos/GetAll', payload: Todo[] };
 
-const todosReducer = (): Todo[] => {
-  return [];
+const setTodos = (todos: Todo[]): GetTodos => (
+  { type: 'todos/GetAll', payload: todos }
+);
+
+export const actions = { getTodos: setTodos };
+
+type Action = GetTodos;
+
+const todosReducer = (
+  todos: Todo[] = [],
+  action: Action,
+): Todo[] => {
+  switch (action.type) {
+    case 'todos/GetAll':
+      return [...todos, ...action.payload];
+
+    default:
+      return todos;
+  }
 };
 
 export default todosReducer;
