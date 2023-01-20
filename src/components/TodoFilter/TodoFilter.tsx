8b9b8541f8bdaa4ad4e8f7633e6d3aa053519520
ysrckr <dyt.yasarcakir@gmail.com>
2023-01-20 15:01:41 +0300

    fix clear button actions

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -9,7 +9,7 @@ export const TodoFilter: React.FC = () => {
   return (
     <form
       className="field has-addons"
-      onSubmit={(event) => event.preventDefault()}
+      onSubmit={event => event.preventDefault()}
     >
       <p className="control">
         <span className="select">
@@ -46,14 +46,20 @@ export const TodoFilter: React.FC = () => {
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              // prettier-ignore
+              onClick={() => dispatch({
+                type: 'filter/CLEAR',
+              })}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
