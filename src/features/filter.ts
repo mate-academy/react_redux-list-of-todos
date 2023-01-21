f2f73be6db16afc5fc84d1acd0597e3df1a40035
dant0n <dmibuc@gmail.com>
2023-01-21 18:49:41 +0200

    Finish hw.

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,50 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type Filter = {
+  query: string,
+  status: Status,
+};
+
+type SetFilterAction = {
+  type: 'filter/SET',
+  payload: Filter,
+};
+
+type UpdateFilterAction = {
+  type: 'filter/UPDATE',
+  payload: Partial<Filter>,
+};
+
+const setFilter = (filter: Filter): SetFilterAction => ({
+  type: 'filter/SET',
+  payload: filter,
+});
+
+const updateFilter = (partialFilter: Partial<Filter>): UpdateFilterAction => ({
+  type: 'filter/UPDATE',
+  payload: partialFilter,
+});
+
+export const actions = { setFilter, updateFilter };
+
+type State = Filter;
+type Action = SetFilterAction | UpdateFilterAction;
+
+const filterReducer = (
+  state: State = { query: '', status: 'all' },
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'filter/SET':
+      return action.payload;
+    case 'filter/UPDATE':
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
