70dbffbd01c90d097ff710f7e36ae48128e96a04
Artem Musii <musiy.artem@gmail.com>
2023-01-31 03:36:52 +0200

    implemented todo list with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { useAppSelector, useAppDispatch } from './app/hooks';
+import { getTodos } from './api';
+import { actions as todosActions } from './features/todos';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getFilteredTodos } from './utils/filterTodos';
 
 export const App: React.FC = () => {
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const [isLoading, setIsLoading] = useState(false);
+  const { query, status } = useAppSelector(state => state.filter);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const fetchedTodos = await getTodos();
+      const visibleTodos = getFilteredTodos(fetchedTodos, query, status);
+
+      dispatch(todosActions.setTodos(visibleTodos));
+      setIsLoading(false);
+    })();
+  }, [query, status]);
+
   return (
     <>
       <div className="section">
@@ -21,14 +41,13 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!isLoading ? <TodoList /> : <Loader />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,51 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetQueryAction = {
+  type: 'filter/QUERY';
+  payload: string;
+};
+
+type SetStatusAction = {
+  type: 'filter/STATUS';
+  payload: Status;
+};
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter/QUERY',
+  payload: query,
+});
+
+const setStatus = (status: Status): SetStatusAction => ({
+  type: 'filter/STATUS',
+  payload: status,
+});
+
+export const actions = { setQuery, setStatus };
+
+type State = {
+  query: string;
+  status: Status;
+};
+
+type Action = SetStatusAction | SetQueryAction;
+
+const initialState = {
+  query: '',
+  status: 'all' as Status,
+};
+
+const filterReducer = (
+  state: State = initialState,
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'filter/QUERY':
+      return { ...state, query: action.payload };
+    case 'filter/STATUS':
+      return { ...state, status: action.payload };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
