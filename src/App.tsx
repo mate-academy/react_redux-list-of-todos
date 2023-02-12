76124a57a0c35af7428cd99356b08f0f6f342be3
Oleksandr Savchenko <baolt.king@gmail.com>
2023-02-12 14:25:21 +0200

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { useDispatch } from 'react-redux';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { actions as todosAction } from './features/todos';
+import { useAppSelector } from './app/hooks';
 
 export const App: React.FC = () => {
+  const [isLoad, setIsLoad] = useState(false);
+  const dispatch = useDispatch();
+
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    setIsLoad(true);
+    const fetchTodos = async () => {
+      const fetchedTodos = await getTodos();
+
+      dispatch(todosAction.setTodos(fetchedTodos));
+    };
+
+    fetchTodos()
+      .finally(() => setIsLoad(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +42,16 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoad
+                ? <Loader />
+                : <TodoList />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal />}
+
     </>
   );
 };
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,30 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type SetTodoAction = {
+  type: 'todos/SET_TODOS',
+  payload: Todo[],
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+type Action = SetTodoAction;
+
+const setTodos = (todos: Todo[]): SetTodoAction => ({
+  type: 'todos/SET_TODOS',
+  payload: todos,
+});
+
+export const actions = { setTodos };
+
+const todosReducer = (
+  state: Todo[] = [],
+  action: Action,
+): Todo[] => {
+  switch (action.type) {
+    case 'todos/SET_TODOS':
+      return action.payload;
+
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
