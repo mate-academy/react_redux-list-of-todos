5126c0569e449356227e545363477ea0cbd28ed2
Svitlana Yudina <osbka31@gmail.com>
2023-02-13 10:34:29 +0200

    develop

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,57 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type FilterOption = {
+  query: string,
+  status: Status | string,
+};
+
+type SelectAction = {
+  type: 'Action / select',
+  payLoad: Status | string,
+};
+
+type QueryAction = {
+  type: 'Action / query',
+  payLoad: string,
+};
+
+const initFilterOption: FilterOption = { query: '', status: Status.All };
+
+const filterBySelect = (status: Status | string): SelectAction => ({
+  type: 'Action / select',
+  payLoad: status,
+});
+const filterByQuery = (currentQuery: string): QueryAction => ({
+  type: 'Action / query',
+  payLoad: currentQuery,
+});
+
+export const filterActions = {
+  filterByQuery,
+  filterBySelect,
+};
+
+const filterReducer = (
+  filter = initFilterOption,
+  action: SelectAction | QueryAction,
+): FilterOption => {
+  switch (action.type) {
+    case 'Action / query':
+      return {
+        ...filter,
+        query: action.payLoad,
+      };
+
+    case 'Action / select': {
+      return {
+        ...filter,
+        status: action.payLoad,
+      };
+    }
+
+    default:
+      return filter;
+  }
 };
 
 export default filterReducer;
--- src/types/Status.ts
+++ src/types/Status.ts
@@ -1,1 +1,6 @@
-export type Status = 'all' | 'active' | 'completed';
+// export type Status = 'all' | 'active' | 'completed';
+export enum Status {
+  All = 'Status/all',
+  Active = 'Status/active',
+  Completed = 'Status/completed',
+}
