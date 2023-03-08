6ef5144ce839042f7b3df0f4b4d87c399252c048
Mohamed Dahi <mohamedjane362@gmail.com>
2023-03-08 11:03:49 +0300

    redux (list of todos)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,34 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions } from './features/todos';
 
 export const App: React.FC = () => {
+  const [isLoadingFailed, setLoadingFailed] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  const dispatch = useAppDispatch();
+
+  const loadTodos = async () => {
+    setLoading(true);
+    try {
+      const todosFromServer = await getTodos();
+
+      dispatch(actions.setTodos(todosFromServer));
+    } catch {
+      setLoadingFailed(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +47,24 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && (
+                <Loader />
+              )}
+
+              {isLoadingFailed && (
+                <p>No todos were loaded!</p>
+              )}
+
+              {!isLoading && !isLoadingFailed && (
+                <TodoList />
+              )}
+
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,50 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type FliterQueryAction = {
+  type: 'query/SET',
+  payload: string,
+};
+
+type FliterStatusAction = {
+  type: 'status/SET',
+  payload: Status,
+};
+
+const setFilterQuery = (query: string): FliterQueryAction => ({
+  type: 'query/SET',
+  payload: query,
+});
+
+const setFilterStatus = (status: Status): FliterStatusAction => ({
+  type: 'status/SET',
+  payload: status,
+});
+
+export const actions = { setFilterQuery, setFilterStatus };
+
+type State = { query: string, status: Status };
+type Action = FliterQueryAction | FliterStatusAction;
+
+const intialState: State = { query: '', status: 'all' };
+
+const filterReducer = (
+  state: State = intialState,
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'query/SET':
+      return {
+        ...state,
+        query: action.payload,
+      };
+    case 'status/SET':
+      return {
+        ...state,
+        status: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
