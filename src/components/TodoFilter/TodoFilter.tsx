e4fdf535d0b7f0f9e265d997317c9b6d4d730126
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-04-27 20:22:54 +0300

    task is done

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { actions } from '../../features/filter';
+import { useAppSelector } from '../../app/hooks';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const { status, query } = useAppSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(actions.setFilter(event.currentTarget.value as Status));
+  };
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(actions.setQuery(event.currentTarget.value));
+  };
+
+  const deleteQuery = () => {
+    dispatch(actions.setQuery(''));
+  };
+
   return (
     <form
       className="field has-addons"
@@ -8,7 +27,11 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            value={status}
+            data-cy="statusSelect"
+            onChange={onSelectChange}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -21,6 +44,8 @@ export const TodoFilter: React.FC = () => {
           data-cy="searchInput"
           type="text"
           className="input"
+          value={query}
+          onChange={onInputChange}
           placeholder="Search..."
         />
         <span className="icon is-left">
@@ -30,6 +55,7 @@ export const TodoFilter: React.FC = () => {
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
           <button
+            onClick={deleteQuery}
             data-cy="clearSearchButton"
             type="button"
             className="delete"
