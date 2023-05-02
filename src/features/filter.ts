420491c8efcd21b949553873f561ccdccb61cde9
Oleksandr Prokopenko <alex.prokop2206@gmail.com>
2023-05-02 09:32:58 +0300

    completed

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,32 @@
-export const actions = { /* put action creators here */};
+type Action = {
+  type: 'filter/FILTER_QUERY' | 'filter/FILTER_STATUS';
+  payload: string;
+};
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+const setFilterQuery = (query: string): Action => ({
+  type: 'filter/FILTER_QUERY',
+  payload: query,
+});
+
+const setFilterStatus = (status: string): Action => ({
+  type: 'filter/FILTER_STATUS',
+  payload: status,
+});
+
+export const actions = { setFilterQuery, setFilterStatus };
+
+const filterReducer = (
+  state = { query: '', status: 'all' },
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'filter/FILTER_QUERY':
+      return { ...state, query: action.payload };
+    case 'filter/FILTER_STATUS':
+      return { ...state, query: action.payload };
+    default:
+      return { ...state };
+  }
 };
 
 export default filterReducer;
