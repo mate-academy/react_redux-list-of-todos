29bba794171ff639b422b874a783beb2ab8c12e6
Kolya Movchan <kmovchan261@gmail.com>
2023-05-21 22:05:37 +0300

    Fix again

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -33,7 +33,7 @@ export const actions = { filterAll, filterActive, filterCompleted };
 
 const todosReducer = (todoList: Todo[] = [], action: Action): Todo[] => {
   const { type, query, todos } = action;
-  const input = query.toLowerCase();
+  const input = query ? query.toLowerCase() : '';
 
   switch (type) {
     case 'todos/ALL':
