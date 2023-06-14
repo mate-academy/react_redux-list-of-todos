3cc38135c769a7f5c264cf2b7b897312c75151ad
Dovhun Artem <dovhun3d@gmail.com>
2023-06-14 20:02:14 +0300

    Add currentTodoReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,48 @@
-export const actions = { /* put action creators here */};
+export enum Status {
+  ALL = 'All',
+  ACTIVE = 'Active',
+  COMPLETED = 'Completed',
+}
 
-const filterReducer = () => {
+export enum StatusAction {
+  ALL = 'filter/StatusAll',
+  ACTIVE = 'filter/Status/Active',
+  COMPLETED = 'filter/StatusCompleted',
+}
+
+type StatusActionType = {
+  type: StatusAction,
+  payload: Status,
+};
+
+const setStatus = (
+  type: StatusAction,
+  status: Status,
+): StatusActionType => ({
+  type,
+  payload: status,
+});
+
+export const actions = {
+  setStatus,
+};
+
+const filterReducer = (
+) => {
   return {
     query: '',
     status: 'all',
   };
 };
+
+// const filterReducer = (
+//   state: State,
+//   action: Action,
+// ): State => {
+//   return {
+//     query: '',
+//     status: 'all',
+//   };
+// };
 
 export default filterReducer;
