a02f8ce256e201ea03055ece9a754aa19cc92291
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2023-06-23 17:28:43 +0300

    solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,52 @@
-export const actions = { /* put action creators here */};
+type FilterState = {
+  query?: string,
+  status?: string
+};
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type ActionQuery = {
+  type: 'filter/QUERY',
+  payload: FilterState,
+};
+
+type ActionSatus = {
+  type: 'filter/STATUS',
+  payload: FilterState,
+};
+
+type Actions = ActionSatus | ActionQuery;
+
+const setQuery = (query: string) :ActionQuery => ({
+  type: 'filter/QUERY',
+  payload: { query },
+});
+
+const setStatus = (status: string) :ActionSatus => ({
+  type: 'filter/STATUS',
+  payload: { status },
+});
+
+export const actions = { setQuery, setStatus };
+
+const firstQuery = { query: '', status: 'all' };
+
+const filterReducer = (
+  state: FilterState = firstQuery,
+  action: Actions,
+) => {
+  switch (action.type) {
+    case 'filter/QUERY':
+      return {
+        ...state,
+        query: action.payload.query,
+      };
+    case 'filter/STATUS':
+      return {
+        ...state,
+        status: action.payload.status,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
