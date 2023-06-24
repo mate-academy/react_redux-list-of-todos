60c8872056e2496ad18ec3bb1f118284c665a570
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-06-24 20:22:35 +0300

    Redux task done

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,49 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatus = {
+  type: 'status/CHANGE';
+  payload: string;
+};
+
+type SetQuery = {
+  type: 'query/CHANGE';
+  payload: string;
+};
+
+const setQuery = (query: string): SetQuery => ({
+  type: 'query/CHANGE',
+  payload: query,
+});
+
+const setStatus = (status: Status): SetStatus => ({
+  type: 'status/CHANGE',
+  payload: status,
+});
+
+export const actions = { setQuery, setStatus };
+
+type Action = SetStatus | SetQuery;
+
+type FilterState = {
+  query: string,
+  status: Status,
+};
+
+const initialState: FilterState = {
+  query: '',
+  status: 'all',
+};
+
+const filterReducer = (state: FilterState = initialState, action: Action) => {
+  switch (action.type) {
+    case 'status/CHANGE':
+      return { ...state, status: action.payload };
+    case 'query/CHANGE':
+      return { ...state, query: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,29 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type GetTodosAction = {
+  type: 'todos/GET',
+  payload: Todo[],
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+const getTodos = (todos: Todo[]): GetTodosAction => ({
+  type: 'todos/GET',
+  payload: todos,
+});
+
+export const actions = { getTodos };
+
+const initialState: Todo[] = [];
+
+type Action = GetTodosAction;
+
+const todosReducer = (state: Todo[] = initialState, action: Action): Todo[] => {
+  switch (action.type) {
+    case 'todos/GET':
+      return action.payload;
+
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
