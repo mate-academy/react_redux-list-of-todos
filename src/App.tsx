56ba12af368e44fe9597d516247bc3e145816a11
GUSILLUS <zebela2002gmail.com>
2023-07-10 15:51:45 +0300

    Redux todo list v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,64 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions as actionTodos } from './features/todos';
 
 export const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const selectTodo = useAppSelector(state => state.currentTodo);
+  const todos = useAppSelector(state => state.todos);
+  const filterState = useAppSelector(state => state.filter);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      setIsLoading(true);
+      try {
+        const todosFromServer = await getTodos();
+
+        dispatch(actionTodos.SetAdd(todosFromServer));
+      } catch (error) {
+        if (error instanceof Error) {
+          setErrorMessage(error.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadTodos();
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    let filteredTodos = [...todos];
+
+    switch (filterState.status) {
+      case 'active':
+        filteredTodos = todos.filter(todo => !todo.completed);
+        break;
+
+      case 'completed':
+        filteredTodos = todos.filter(todo => todo.completed);
+        break;
+
+      default:
+        break;
+    }
+
+    if (filterState.query) {
+      const lowerCaseQuery = filterState.query.toLowerCase();
+
+      filteredTodos = filteredTodos
+        .filter(todo => todo.title.toLowerCase().includes(lowerCaseQuery));
+    }
+
+    return filteredTodos;
+  }, [todos, filterState]);
+
   return (
     <>
       <div className="section">
@@ -21,14 +77,19 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList todos={visibleTodos} errorMessage={errorMessage} />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectTodo && (
+        <TodoModal />
+      )}
     </>
   );
 };
