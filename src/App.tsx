e5c072225eb78d79437de9e26be465c7b63aa8a0
Kate <novak5683@gmail.com>
2023-07-12 15:13:36 +0200

    todo list done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,7 +8,71 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { actions as todosActions } from './features/todos';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { getTodos } from './api';
+import { SortType } from './types/SortType';
+import { Todo } from './types/Todo';
+
+const getFilteredTodos = (
+  todos: Todo[],
+  query: string,
+  statusFilter: SortType,
+): Todo[] => {
+  let newTodos = [...todos];
+
+  if (query) {
+    const newQuery = query.toLowerCase().trim();
+
+    newTodos = newTodos.filter(
+      todo => todo.title.toLowerCase().includes(newQuery),
+    );
+  }
+
+  switch (statusFilter) {
+    case SortType.ACTIVE:
+      newTodos = newTodos.filter(todo => !todo.completed);
+      break;
+    case SortType.COMPLETED:
+      newTodos = newTodos.filter(todo => todo.completed);
+      break;
+    case SortType.ALL:
+    default:
+      break;
+  }
+
+  return newTodos;
+};
+
 export const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector(state => state.todos);
+  const { query, status } = useAppSelector(state => state.filter);
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+  const [isLoading, setisLoading] = useState(true);
+  const [hasError, sethasError] = useState(false);
+
+  const visibleTodos = useMemo(
+    () => getFilteredTodos(todos, query, status),
+    [todos, query, status],
+  );
+
+  const getTodosServer = async () => {
+    try {
+      const arrayTodos = await getTodos();
+
+      dispatch(todosActions.setTodos(arrayTodos));
+      setisLoading(false);
+      sethasError(false);
+    } catch (error) {
+      sethasError(true);
+    }
+  };
+
+  useEffect(() => {
+    getTodosServer();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +85,24 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+
+              {hasError
+                ? <p>Error, server is unavailable</p>
+                : <TodoList todos={visibleTodos} />}
             </div>
           </div>
+          {!isLoading && (
+            <p className="notification is-warning">
+              There are no todos matching current filter criteria
+            </p>
+          )}
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal />
+      )}
     </>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,53 @@
-export const actions = { /* put action creators here */};
+import { SortType } from '../types/SortType';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetQueryAction = { type: 'filter/SET_QUERY', payload: string };
+type SetStatusAction = { type: 'filter/SET_STATUS', payload: SortType };
+type SetClearAction = { type: 'filter/SET_CLEAR' };
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter/SET_QUERY',
+  payload: query,
+});
+
+const setStatus = (status: SortType): SetStatusAction => ({
+  type: 'filter/SET_STATUS',
+  payload: status,
+});
+
+const setClear = (): SetClearAction => ({
+  type: 'filter/SET_CLEAR',
+});
+
+export const actions = { setClear, setQuery, setStatus };
+
+type Action = SetClearAction | SetQueryAction | SetStatusAction;
+type State = { query: string, status: SortType };
+
+const filterInitial: State = {
+  query: '',
+  status: SortType.ALL,
+};
+
+const filterReducer = (state = filterInitial, actoin: Action): State => {
+  switch (actoin.type) {
+    case 'filter/SET_CLEAR':
+      return {
+        ...state,
+        query: '',
+      };
+    case 'filter/SET_QUERY':
+      return {
+        ...state,
+        query: actoin.payload,
+      };
+    case 'filter/SET_STATUS':
+      return {
+        ...state,
+        status: actoin.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
