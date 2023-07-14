4818c836d351ac561246e0d6a960f181ae106d16
Yelyzaveta <molochiza@gmail.com>
2023-07-14 15:54:10 +0300

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,26 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as FilterActions } from '../../features/filter';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { query, status } = useAppSelector(state => state.filter);
+
+  const handleChangeQuery = (value: string) => (
+    dispatch(FilterActions.setQuery(value)));
+
+  const handleChangeStatus = (value: Status) => (
+    dispatch(FilterActions.setStatus(value)));
+
+  const handleClear = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    handleChangeQuery('');
+    handleChangeStatus('all');
+  };
+
   return (
     <form
       className="field has-addons"
@@ -8,7 +28,11 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            value={status}
+            onChange={(e) => handleChangeStatus(e.target.value as Status)}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -22,19 +46,23 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={query}
+          onChange={(e) => handleChangeQuery(e.target.value)}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleClear}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
