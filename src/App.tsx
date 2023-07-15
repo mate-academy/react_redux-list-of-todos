41751609226309fb2971e503fb9f2898a392a86c
Kateryna Shylina <katya.dhilina@gmail.com>
2023-07-15 16:33:27 +0300

    solution :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions as todosActions } from './features/todos';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { Filter } from './types/Filter';
+import { TodoModal } from './components/TodoModal';
+
+const visibleTodos = (query: string, todos: Todo[], status: string) => {
+  const formattedQuery = query.trim().toLowerCase();
+
+  switch (status) {
+    case Filter.Active:
+      return todos.filter(
+        todo => !todo.completed && todo.title.toLowerCase().includes(formattedQuery),
+      );
+    case Filter.Completed:
+      return todos.filter(
+        todo => todo.completed && todo.title.toLowerCase().includes(formattedQuery),
+      );
+    default:
+      return todos.filter(
+        todo => todo.title.toLowerCase().includes(formattedQuery),
+      );
+  }
+};
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector(state => state.todos);
+  const { query, status } = useAppSelector(state => state.filter);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  const visibleTodoItems = visibleTodos(query, todos, status);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then((todoItems) => {
+        dispatch(todosActions.set(todoItems));
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [dispatch]);
+
   return (
     <>
       <div className="section">
@@ -21,14 +65,17 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (<Loader />) : (
+                <TodoList
+                  todos={visibleTodoItems}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
