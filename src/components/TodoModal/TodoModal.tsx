6d50f942747cda792d160917730c613e17cb30ab
Serhii Melnychenko <8melya8@gmail.com>
2023-07-15 18:20:11 +0300

    Solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,40 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { useAppSelector } from '../../app/hooks';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  userId: number,
+  handleCloseModal: () => void,
+};
+
+export const TodoModal: React.FC<Props> = ({ userId, handleCloseModal }) => {
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    getUser(userId).then(res => setUser(res));
+  }, [userId]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {user && currentTodo ? (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${currentTodo.id}`}
+            </div>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+            <button
+              aria-label="none"
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={handleCloseModal}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">{currentTodo.title}</p>
+
+            <p className="block" data-cy="modal-user">
+              {!currentTodo.completed
+                ? (<strong className="has-text-danger">Planned</strong>)
+                : (<strong className="has-text-success">Done</strong>)}
+
+              {' by '}
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
         </div>
-      </div>
+      ) : (<Loader />)}
     </div>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,56 @@
-export const actions = { /* put action creators here */};
+import { Filter } from '../components/enum/Filter';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatusAction = {
+  type: 'filter/SET_STATUS',
+  payload: Filter,
+};
+
+type QueryAction = {
+  type: 'filter/SET_QUERY',
+  payload: string,
+};
+
+type ClearAction = {
+  type: 'filter/CLEAR'
+};
+
+const setStatus = (payload: Filter): SetStatusAction => ({
+  type: 'filter/SET_STATUS',
+  payload,
+});
+
+const setQuery = (payload: string): QueryAction => ({
+  type: 'filter/SET_QUERY',
+  payload,
+});
+
+const clear = (): ClearAction => ({ type: 'filter/CLEAR' });
+
+export const actions = { setStatus, setQuery, clear };
+
+type State = {
+  query: string;
+  status: Filter;
+};
+
+const initialState = { query: '', status: Filter.All };
+
+type Action = SetStatusAction | QueryAction | ClearAction;
+
+const filterReducer = (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case 'filter/SET_STATUS':
+      return { ...state, status: action.payload };
+
+    case 'filter/SET_QUERY':
+      return { ...state, query: action.payload };
+
+    case 'filter/CLEAR':
+      return { ...state, query: '' };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
