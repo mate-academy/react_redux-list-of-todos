388c19386f26f21e85b2f39f038f9fdabb1638bf
Serhii Vorobei <s.vorobei.dev@gmail.com>
2023-07-21 13:26:43 +0300

    Reack redux todos have been implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { getTodos } from './api';
+import { actions } from './features/todos';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const [isLoader, setIsLoader] = useState<boolean>(false);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    setIsLoader(true);
+    getTodos()
+      .then((res) => {
+        dispatch(actions.setTodos(res));
+      })
+      .finally(() => {
+        setIsLoader(false);
+      });
+  }, []);
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
-            <h1 className="title">Todos:</h1>
-
-            <div className="block">
-              <TodoFilter />
-            </div>
-
-            <div className="block">
+            {isLoader ? (
               <Loader />
-              <TodoList />
-            </div>
+            ) : (
+              <>
+                <h1 className="title">Todos:</h1>
+
+                <div className="block">
+                  <TodoFilter />
+                </div>
+
+                <div className="block">
+                  <TodoList />
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && (
+        <TodoModal />
+      )}
     </>
   );
 };
