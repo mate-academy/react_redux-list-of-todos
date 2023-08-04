841cb745848e33b070974bf480f54cf0ea6e3bbe
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-08-04 10:12:47 +0300

    add task solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,47 @@
-export const actions = { /* put action creators here */};
+type AllActions = { type: 'ALL', payload: string };
+type ActiveActions = { type: 'ACTIVE', payload: string };
+type CompletedActions = { type: 'COMPLETED', payload: string };
+type InputActions = { type: 'INPUT', payload: string };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type Action = AllActions | ActiveActions | CompletedActions | InputActions;
+
+const all = (query: string): Action => ({ type: 'ALL', payload: query });
+const active = (query: string): Action => ({ type: 'ACTIVE', payload: query });
+const completed = (query: string): Action => (
+  { type: 'COMPLETED', payload: query });
+const input = (query: string): Action => ({ type: 'INPUT', payload: query });
+
+export const actions = {
+  all, active, completed, input,
+};
+
+const initialState = { query: '', status: 'all' };
+
+const filterReducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case 'ALL':
+      return {
+        query: action.payload,
+        status: 'all',
+      };
+    case 'ACTIVE':
+      return {
+        query: action.payload,
+        status: 'active',
+      };
+    case 'COMPLETED':
+      return {
+        query: action.payload,
+        status: 'completed',
+      };
+    case 'INPUT':
+      return {
+        ...state,
+        query: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
