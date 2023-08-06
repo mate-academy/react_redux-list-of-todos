9e1ae6abbf6f8d85baedceeb06d594bb6845a66f
Illia Vinkov <illia.vinkov@gmail.com>
2023-08-06 09:12:02 +0200

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { actions as filterAction } from '../../features/filter';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const filter = useAppSelector(state => state.filter);
+
+  const setFilter = (value: string) => {
+    switch (value) {
+      case Status.Active:
+        return dispatch(filterAction.active(filter.query));
+      case Status.Completed:
+        return dispatch(filterAction.completed(filter.query));
+      default:
+        return dispatch(filterAction.all(filter.query));
+    }
+  };
+
+  const setInput = (value: string) => {
+    return dispatch(filterAction.input(value));
+  };
+
   return (
     <form
       className="field has-addons"
@@ -8,20 +30,24 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            onChange={(e) => setFilter(e.target.value)}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
           </select>
         </span>
       </p>
-
       <p className="control is-expanded has-icons-left has-icons-right">
         <input
           data-cy="searchInput"
           type="text"
           className="input"
           placeholder="Search..."
+          value={filter.query}
+          onChange={(e) => setInput(e.target.value)}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
@@ -29,11 +55,15 @@ export const TodoFilter: React.FC = () => {
 
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
+          {filter.query && (
+            <button
+              data-cy="clearSearchButton"
+              aria-label="delete"
+              type="button"
+              className="delete"
+              onClick={() => setInput('')}
+            />
+          )}
         </span>
       </p>
     </form>
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,49 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type AllActions = { type: 'ALL', payload: string };
+type ActiveActions = { type: 'ACTIVE', payload: string };
+type CompletedActions = { type: 'COMPLETED', payload: string };
+type InputActions = { type: 'INPUT', payload: string };
+
+type Action = AllActions | ActiveActions | CompletedActions | InputActions;
+
+const all = (query: string): Action => ({ type: 'ALL', payload: query });
+const active = (query: string): Action => ({ type: 'ACTIVE', payload: query });
+const completed = (query: string): Action => (
+  { type: 'COMPLETED', payload: query });
+const input = (query: string): Action => ({ type: 'INPUT', payload: query });
+
+export const actions = {
+  all, active, completed, input,
+};
+
+const initialState = { query: '', status: Status.All };
+
+const filterReducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case 'ALL':
+      return {
+        query: action.payload,
+        status: Status.All,
+      };
+    case 'ACTIVE':
+      return {
+        query: action.payload,
+        status: Status.Active,
+      };
+    case 'COMPLETED':
+      return {
+        query: action.payload,
+        status: Status.Completed,
+      };
+    case 'INPUT':
+      return {
+        ...state,
+        query: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
