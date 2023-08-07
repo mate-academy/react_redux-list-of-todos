c0315f7b145152b2f6c80a3294bd05d3e8b1fe21
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2023-08-07 15:59:33 +0300

    Solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as filterAactions } from '../../features/filter';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const filter = useAppSelector(state => state.filter);
+
+  const onStatusSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(filterAactions.setStatus(event.target.value as Status));
+  };
+
+  const onQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(filterAactions.setQuery(event.target.value));
+  };
+
+  const onDeleteQuery = () => dispatch(filterAactions.clearQuery());
+
   return (
     <form
       className="field has-addons"
@@ -8,7 +24,11 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            value={filter.status}
+            onChange={onStatusSelect}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -22,19 +42,24 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={filter.query}
+          onChange={onQuery}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {filter.query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={onDeleteQuery}
+              aria-label="delete"
+            />
+          </span>
+        )}
       </p>
     </form>
   );
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,37 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type SetTodosAction = {
+  type: 'todos/SET';
+  payload: Todo[];
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+type ClearTodosAction = { type: 'todos/CLEAR' };
+
+const setTodos = (todos: Todo[]): SetTodosAction => ({
+  type: 'todos/SET',
+  payload: todos,
+});
+
+const clear = (): ClearTodosAction => ({ type: 'todos/CLEAR' });
+
+type Action = SetTodosAction | ClearTodosAction;
+
+export const actions = { setTodos, clear };
+
+const todosReducer = (
+  todos: Todo[] = [],
+  action: Action,
+): Todo[] => {
+  switch (action.type) {
+    case 'todos/SET':
+      return [...todos, ...action.payload];
+
+    case 'todos/CLEAR':
+      return [];
+
+    default:
+      return todos;
+  }
 };
 
 export default todosReducer;
