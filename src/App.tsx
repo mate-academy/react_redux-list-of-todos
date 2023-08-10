c20e1a9ff0e710f57f9d97fbecaa695071aa5721
Gabriella Ivanjuk <gabriella.ivanjuk@gmail.com>
2023-08-10 18:50:41 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,60 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions as todosActions } from './features/todos';
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const status = useAppSelector(state => state.filter.status);
+  const query = useAppSelector(state => state.filter.query);
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const isSelected = currentTodo !== null;
+
+  const loadTodos = async () => {
+    setIsLoading(true);
+    let allTodos;
+
+    try {
+      allTodos = await getTodos();
+    } catch (error) {
+      throw new Error('Data loading error');
+    }
+
+    switch (status) {
+      case 'all':
+        break;
+
+      case 'active':
+        allTodos = allTodos.filter(todo => !todo.completed);
+        break;
+
+      case 'completed':
+        allTodos = allTodos.filter(todo => todo.completed);
+        break;
+
+      default:
+        break;
+    }
+
+    const lowerQuery = query.toLowerCase();
+
+    allTodos = allTodos.filter((todo) => {
+      const lowerTitle = todo.title.toLowerCase();
+
+      return lowerTitle.includes(lowerQuery);
+    });
+
+    dispatch(todosActions.setTodos(allTodos));
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadTodos();
+  }, [query, status]);
+
   return (
     <>
       <div className="section">
@@ -21,14 +73,13 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? <Loader /> : <TodoList />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isSelected && <TodoModal />}
     </>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,49 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetQueryAction = {
+  type: 'query/SET',
+  payload: string,
+};
+
+type SetStatusAction = {
+  type: 'status/SET',
+  payload: Status,
+};
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'query/SET',
+  payload: query,
+});
+
+const setStatus = (status: Status): SetStatusAction => ({
+  type: 'status/SET',
+  payload: status,
+});
+
+export const actions = { setQuery, setStatus };
+
+type Action = SetQueryAction | SetStatusAction;
+type State = { query: string, status: Status };
+
+const initialState: State = {
+  query: '',
+  status: 'all',
+};
+
+const filterReducer = (
+  state = initialState,
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'query/SET':
+      return { ...state, query: action.payload };
+
+    case 'status/SET':
+      return { ...state, status: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
