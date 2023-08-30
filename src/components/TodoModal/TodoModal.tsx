cabcb2b8637819cf18251a02a4c0feb00e58c520
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-08-30 17:57:13 +0300

    solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,40 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
+import { actions as currentTodoActions } from '../../features/currentTodo';
+import { ErrorModal } from '../ErrorModal';
 
-export const TodoModal: React.FC = () => {
+type Props = {
+  currentTodo: Todo;
+};
+
+export const TodoModal: React.FC<Props> = ({ currentTodo }) => {
+  const [userInfo, setUserInfo] = useState<User | null>(null);
+  const [errorStatus, setErrorStatus] = useState('');
+  const [isLoader, setIsLoader] = useState(false);
+  const {
+    userId,
+    id,
+    title,
+    completed,
+  } = currentTodo;
+
+  const { name, email } = userInfo || {};
+
+  const loadDataUser = async (currentUserId: number) => {
+    try {
+      setIsLoader(true);
+      const data = await getUser(currentUserId);
+
+      setUserInfo(data);
+    } catch {
+      setErrorStatus(' please try again later');
+    } finally {
+      setIsLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    loadDataUser(userId);
+  }, [userId]);
+
+  const dispatch = useDispatch();
+  const closeTodo = () => dispatch(currentTodoActions.removeTodo());
+
+  const closeError = () => {
+    setErrorStatus('');
+    closeTodo();
+  };
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {isLoader && <Loader />}
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+      {!!errorStatus.length && (
+        <ErrorModal btnAction={closeError} errorText={errorStatus} />
+      )}
+
+      {userInfo && (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${id}`}
+            </div>
+
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => closeTodo()}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">{title}</p>
+
+            <p className="block" data-cy="modal-user">
+              {completed
+                ? (<strong className="has-text-success">Done</strong>)
+                : (<strong className="has-text-danger">Planned</strong>)}
+              {' by '}
+              <a href={`mailto:${email}`}>{name}</a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,47 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetQueryAction = {
+  type: 'filter/SET_QUERY';
+  payload: string;
+};
+
+type SetStatusAction = {
+  type: 'filter/SET_STATUS';
+  payload: Status;
+};
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter/SET_QUERY',
+  payload: query,
+});
+
+const setStatus = (status: Status): SetStatusAction => ({
+  type: 'filter/SET_STATUS',
+  payload: status,
+});
+
+export const actions = { setQuery, setStatus };
+
+type Filter = {
+  query: string;
+  status: Status;
+};
+type Action = SetQueryAction | SetStatusAction;
+
+const startFilters: Filter = { query: '', status: Status.All };
+
+const filterReducer = (
+  filter: Filter = startFilters,
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'filter/SET_QUERY':
+      return { ...filter, query: action.payload };
+    case 'filter/SET_STATUS':
+      return { ...filter, status: action.payload };
+    default:
+      return filter;
+  }
 };
 
 export default filterReducer;
