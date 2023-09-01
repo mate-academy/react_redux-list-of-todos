e94d3654d20c7ea763fe8805c22d18e6394f54a5
Eugene Kulish <genekulish13@gmail.com>
2023-09-01 16:36:32 +0300

    solve the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable max-len */
 import React from 'react';
+import { Provider } from 'react-redux';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -8,9 +8,17 @@ import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
+import { store } from './app/store';
+import { useAppSelector } from './app/hooks';
+
+import { Todo } from './types/Todo';
+
 export const App: React.FC = () => {
+  const todos: Todo[] = useAppSelector(state => state.todos);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
   return (
-    <>
+    <Provider store={store}>
       <div className="section">
         <div className="container">
           <div className="box">
@@ -21,14 +29,19 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
+              {!todos.length && (
+                <Loader />
+              )}
+
               <TodoList />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
-    </>
+      {currentTodo && (
+        <TodoModal />
+      )}
+    </Provider>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,44 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetQueryAction = { type: 'filter/setQuery', payload: string };
+type ClearQueryAction = { type: 'filter/clearQuery' };
+type SetStatusAction = { type: 'filter/setStatus', payload: Status };
+
+type FilterAction = SetQueryAction | ClearQueryAction | SetStatusAction;
+
+const setQuery = (value: string): SetQueryAction => ({
+  type: 'filter/setQuery',
+  payload: value,
+});
+
+const clearQuery = (): ClearQueryAction => ({
+  type: 'filter/clearQuery',
+});
+
+const setStatus = (value: Status): SetStatusAction => ({
+  type: 'filter/setStatus',
+  payload: value,
+});
+
+export const actions = { setQuery, setStatus, clearQuery };
+
+const filterReducer = (
+  state = { query: '', status: Status.All },
+  action: FilterAction,
+) => {
+  switch (action.type) {
+    case 'filter/setQuery':
+      return { ...state, query: action.payload };
+
+    case 'filter/clearQuery':
+      return { ...state, query: '' };
+
+    case 'filter/setStatus':
+      return { ...state, status: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
