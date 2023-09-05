4156447e1e390cf7f08dce5b7d062d7ed386cdb4
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-09-06 00:22:19 +0200

    solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,38 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type QueryAction = { type: 'add/query', payload: string };
+type StatusAction = { type: 'add/status', payload: string };
+type ClearAction = { type: 'add/clear' };
+
+type Actions = QueryAction | StatusAction | ClearAction;
+
+const addQuery = (query: string) => ({ type: 'add/query', payload: query });
+const addStatus = (status: Status) => ({ type: 'add/status', payload: status });
+const clearQuery = () => ({ type: 'add/clear' });
+
+export const actions = { addQuery, addStatus, clearQuery };
+
+type Param = {
+  query: string,
+  status: Status,
+};
+
+const initialParams: Param = { query: '', status: 'all' };
+
+const filterReducer = (
+  state: Param = initialParams,
+  action: Actions,
+): Param => {
+  switch (action.type) {
+    case 'add/query':
+      return { ...state, query: action.payload };
+    case 'add/status':
+      return { ...state, status: action.payload as Status };
+    case 'add/clear':
+      return { ...state, query: '' };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,19 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type AddTodos = { type: 'todos/ADD', payload: Todo[] };
 
-const todosReducer = (): Todo[] => {
-  return [];
+const addTodos = (todos: Todo[]): AddTodos => (
+  { type: 'todos/ADD', payload: todos }
+);
+
+export const actions = { addTodos };
+
+const todosReducer = (todos: Todo[] = [], action: AddTodos): Todo[] => {
+  if (action.type === 'todos/ADD') {
+    return action.payload;
+  }
+
+  return todos;
 };
 
 export default todosReducer;
