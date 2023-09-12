99a42f1470a9d3853d3db32334fcde8acdf805a8
Dima Astapenko <bonum_taurus@ukr.net>
2023-09-12 15:07:33 +0300

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,28 +7,46 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions as todosActions } from './features/todos';
 
 export const App: React.FC = () => {
+  const [isSpinner, setIsSpinner] = useState(true);
+  const dispatch = useAppDispatch();
+  const todo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    getTodos()
+      .then(todos => dispatch(todosActions.setTodos(todos)))
+      .finally(() => setIsSpinner(false));
+  }, []);
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
-            <h1 className="title">Todos:</h1>
-
-            <div className="block">
-              <TodoFilter />
-            </div>
-
-            <div className="block">
+            {isSpinner ? (
               <Loader />
-              <TodoList />
-            </div>
+            ) : (
+              <>
+                <h1 className="title">Todos:</h1>
+
+                <div className="block">
+                  <TodoFilter />
+                </div>
+
+                <div className="block">
+                  <TodoList />
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {todo && <TodoModal />}
     </>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,46 @@
-export const actions = { /* put action creators here */};
+type ChangeStatus = { type: 'filter/STATUS', payload: string };
+type ChangeByQuery = { type: 'filter/QUERY', payload: string };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type Action = ChangeStatus | ChangeByQuery;
+
+const changeStatus = (status: string): ChangeStatus => ({
+  type: 'filter/STATUS', payload: status,
+});
+const changeByQuery = (query: string): ChangeByQuery => ({
+  type: 'filter/QUERY', payload: query,
+});
+
+export const actions = { changeStatus, changeByQuery };
+
+type ParamsFilter = {
+  query: string,
+  status: string,
+};
+
+const initialParams: ParamsFilter = {
+  query: '',
+  status: 'all',
+};
+
+const filterReducer = (params = initialParams, action: Action) => {
+  switch (action.type) {
+    case 'filter/STATUS': {
+      return {
+        ...params,
+        status: action.payload,
+      };
+    }
+
+    case 'filter/QUERY': {
+      return {
+        ...params,
+        query: action.payload,
+      };
+    }
+
+    default:
+      return params;
+  }
 };
 
 export default filterReducer;
