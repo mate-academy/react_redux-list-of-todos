8ee7b464f8be43fcb442613401c4f78cf87edfb7
Horizon-git <apbreloaded947@gmail.com>
2023-09-12 17:32:50 +0300

    completed task

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,40 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUser } from '../../api';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as currentTodoActions } from '../../features/currentTodo';
+import { User } from '../../types/User';
 import { Loader } from '../Loader';
 
 export const TodoModal: React.FC = () => {
+  const currentTodo = useAppSelector((state) => state.currentTodo);
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (currentTodo) {
+      getUser(currentTodo.userId)
+        .then(setUser)
+        .catch((error) => {
+          throw new Error(error);
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }, []);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${currentTodo?.id}`}
+            </div>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => dispatch(currentTodoActions.removeTodo())}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {currentTodo?.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {currentTodo?.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
+
+              {' by '}
+
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,54 @@
-export const actions = { /* put action creators here */};
+import { Filter } from '../types/Filter';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetFilterTypeAction = {
+  type: 'filter/SET_FILTER_TYPE';
+  payload: string;
+};
+
+type SetQueryAction = {
+  type: 'filter/SET_QUERY';
+  payload: string;
+};
+
+type ClearQueryAction = { type: 'filter/CLEAR_QUERY' };
+
+const setFilterType = (type: string): SetFilterTypeAction => ({
+  type: 'filter/SET_FILTER_TYPE',
+  payload: type,
+});
+
+const setQuery = (type: string): SetQueryAction => ({
+  type: 'filter/SET_QUERY',
+  payload: type,
+});
+
+const clearQuery = (): ClearQueryAction => ({ type: 'filter/CLEAR_QUERY' });
+
+export const actions = { setFilterType, setQuery, clearQuery };
+
+type State = {
+  query: string,
+  filterType: string
+};
+
+const initialState: State = { query: '', filterType: Filter.ALL };
+
+type Action = SetFilterTypeAction | SetQueryAction | ClearQueryAction;
+
+const filterReducer = (
+  state: State = initialState,
+  action: Action,
+): State => {
+  switch (action.type) {
+    case 'filter/SET_FILTER_TYPE':
+      return { ...state, filterType: action.payload };
+    case 'filter/SET_QUERY':
+      return { ...state, query: action.payload };
+    case 'filter/CLEAR_QUERY':
+      return { ...state, query: '' };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
