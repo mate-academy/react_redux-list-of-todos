8f0326b9e88f2aa8762f3c43796b95c7eb2e1b40
IlonaBorkovska <borkovska.io@gmail.com>
2023-09-15 21:06:47 +0300

    add task solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,58 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type FilterAction = {
+  type: 'filterTodos/STATUS';
+  payload: Status;
+};
+
+type FindAction = {
+  type: 'filterTodos/QUERY';
+  payload: string;
+};
+
+const filterTodos = (status: Status): FilterAction => ({
+  type: 'filterTodos/STATUS',
+  payload: status,
+});
+
+const findTodos = (query: string): FindAction => ({
+  type: 'filterTodos/QUERY',
+  payload: query,
+});
+
+export const actions = { filterTodos, findTodos };
+
+type Action = FilterAction | FindAction;
+
+type State = {
+  query: string,
+  status: Status,
+};
+
+const initState = {
+  query: '',
+  status: Status.ALL,
+};
+
+const filterReducer = (
+  state = initState,
+  action: Action,
+): State => {
+  switch (action.type) {
+    case 'filterTodos/STATUS':
+      return {
+        ...state,
+        status: action.payload,
+      };
+
+    case 'filterTodos/QUERY':
+      return {
+        ...state,
+        query: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,75 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type SetAction = {
+  type: 'todos/SET',
+  payload: Todo[],
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+type SetLoadingAction = {
+  type: 'todos/SET_LOADING',
+  payload: boolean,
+};
+
+type SetErrorAction = {
+  type: 'todos/SET_ERROR',
+  payload: string,
+};
+
+const set = (todos: Todo[]): SetAction => ({
+  type: 'todos/SET',
+  payload: todos,
+});
+
+const setLoading = (isLoading: boolean): SetLoadingAction => ({
+  type: 'todos/SET_LOADING',
+  payload: isLoading,
+});
+
+const setError = (error: string): SetErrorAction => ({
+  type: 'todos/SET_ERROR',
+  payload: error,
+});
+
+type State = {
+  loading: boolean,
+  todos: Todo[],
+  error: string,
+};
+
+const initState = {
+  loading: false,
+  todos: [],
+  error: '',
+};
+
+type Action = SetAction | SetLoadingAction | SetErrorAction;
+
+export const actions = { set, setLoading, setError };
+
+const todosReducer = (
+  state = initState,
+  action: Action,
+): State => {
+  switch (action.type) {
+    case 'todos/SET':
+      return {
+        ...state,
+        todos: action.payload,
+      };
+    case 'todos/SET_LOADING':
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case 'todos/SET_ERROR':
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
