4d3fd62855c413df243977ffcd5f17812581f328
Mykhailo Shevchuk <mishashevchuk@gmail.com>
2023-10-04 22:59:25 +0300

    initial commit

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,56 @@
-export const actions = { /* put action creators here */};
+import { Filter } from '../types/Filter';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatusAction = {
+  type: 'filter/SET_STATUS',
+  payload: Filter,
+};
+
+type SetQueryAction = {
+  type: 'filter/SET_QUERY',
+  payload: string,
+};
+
+type ClearQueryAction = {
+  type: 'filter/CLEAR'
+};
+
+const setStatus = (payload: Filter): SetStatusAction => ({
+  type: 'filter/SET_STATUS',
+  payload,
+});
+
+const setQuery = (payload: string): SetQueryAction => ({
+  type: 'filter/SET_QUERY',
+  payload,
+});
+
+const clearQuery = (): ClearQueryAction => ({ type: 'filter/CLEAR' });
+
+export const actions = { setStatus, setQuery, clearQuery };
+
+type State = {
+  query: string;
+  status: Filter;
+};
+
+const initialState = { query: '', status: Filter.All };
+
+type Action = SetStatusAction | SetQueryAction | ClearQueryAction;
+
+const filterReducer = (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case 'filter/SET_STATUS':
+      return { ...state, status: action.payload };
+
+    case 'filter/SET_QUERY':
+      return { ...state, query: action.payload };
+
+    case 'filter/CLEAR':
+      return { ...state, query: '' };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
