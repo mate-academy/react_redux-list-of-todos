c54aef3f7d4d56b46ad0ab3f6876a6fa8c8ad894
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-10-06 18:18:25 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,24 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { useAppSelector } from './app/hooks';
+import { useDispatch } from 'react-redux';
+import { getTodos } from './api';
+import { actions as todosAction } from './features/todos';
 
 export const App: React.FC = () => {
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then((todos) => dispatch(todosAction.addTodos(todos)))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +37,18 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList />
+              )}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {currentTodo && (
+        <TodoModal />
+      )}
     </>
   );
 };
--- src/types/Status.ts
+++ src/types/Status.ts
@@ -1,1 +1,5 @@
-export type Status = 'all' | 'active' | 'completed';
+export enum Status {
+  ALL = 'ALL',
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
+};
