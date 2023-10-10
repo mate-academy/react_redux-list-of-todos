55e7d9782fb5996419a3306dd05ec852c03171ea
Emma Karpets <e.karpets.job@gmail.com>
2023-10-10 15:12:52 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,52 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { todosAction } from './features/todos';
+import { currentTodoActions } from './features/currentTodo';
+import { filterActions } from './features/filter';
+import { Status } from './types/Status';
 
 export const App: React.FC = () => {
+  const [openTodo, setOpenTodo] = useState<boolean>(false);
+  const [crossClicked, setCrossClicked] = useState<boolean>(false);
+
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector(state => state.todos);
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+  const { query, status } = useAppSelector(state => state.filter);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const todosFromServer = await getTodos();
+
+      dispatch(todosAction.setTodos(todosFromServer));
+    };
+
+    fetchData();
+  }, []);
+
+  const onChosenFilter = (data: Status) => {
+    dispatch(filterActions.setStatus(data));
+  };
+
+  const onQuery = (data: string) => {
+    dispatch(filterActions.setQuery(data));
+  };
+
+  const onChosenTodo = (todo: Todo) => {
+    dispatch(currentTodoActions.setTodo(todo));
+    setOpenTodo(true);
+    setCrossClicked(true);
+  };
+
+  const todoModalClick = () => {
+    setOpenTodo(false);
+    setCrossClicked(false);
+  };
+
   return (
     <>
       <div className="section">
@@ -17,18 +61,36 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                onChosenFilter={onChosenFilter}
+                onQuery={onQuery}
+                query={query}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {todos.length === 0
+                ? <Loader />
+                : (
+                  <TodoList
+                    todos={todos}
+                    filter={status}
+                    query={query}
+                    onChosenTodo={onChosenTodo}
+                    crossClicked={crossClicked}
+                  />
+                )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      { selectedTodo && openTodo && (
+        <TodoModal
+          todo={selectedTodo}
+          handleDeleteClick={todoModalClick}
+        />
+      )}
     </>
   );
 };
