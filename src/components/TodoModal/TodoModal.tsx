31a3341b05cb0cf4f8032d44a90a735d6e173fec
Nikita Orliak <nikitonorl@gmail.com>
2023-10-14 17:14:40 +0200

    Add task solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,40 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as currentTodoActions } from '../../features/currentTodo';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
 
 export const TodoModal: React.FC = () => {
+  const [isLoader, setIsLoader] = useState<boolean>(false);
+  const [user, setUser] = useState<User>();
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    setIsLoader(true);
+
+    if (currentTodo) {
+      getUser(currentTodo.userId)
+        .then(setUser)
+        .finally(() => setIsLoader(false));
+    }
+  }, []);
+
+  const handleRemoveCurrentTodo = () => {
+    dispatch(currentTodoActions.removeTodo());
+  };
+
   return (
-    <div className="modal is-active" data-cy="modal">
-      <div className="modal-background" />
+    <>
+      {
+        currentTodo && (
+          <div className="modal is-active" data-cy="modal">
+            <div className="modal-background" />
 
-      <Loader />
+            {
+              isLoader && (
+                <Loader />
+              )
+            }
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+            {
+              user && (
+                <div className="modal-card">
+                  <header className="modal-card-head">
+                    <div
+                      className="modal-card-title has-text-weight-medium"
+                      data-cy="modal-header"
+                    >
+                      {`Todo #${currentTodo.id}`}
+                    </div>
+
+                    {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                    <button
+                      type="button"
+                      className="delete"
+                      data-cy="modal-close"
+                      onClick={handleRemoveCurrentTodo}
+                    />
+                  </header>
+
+                  <div className="modal-card-body">
+                    <p
+                      className="block"
+                      data-cy="modal-title"
+                    >
+                      {currentTodo.title}
+                    </p>
+
+                    <p className="block" data-cy="modal-user">
+                      {
+                        currentTodo.completed
+                          ? (
+                            <strong className="has-text-success">Done</strong>
+                          ) : (
+                            <strong className="has-text-danger">Planned</strong>
+                          )
+                      }
+                      {' by '}
+                      <a href={`mailto:${user.email}`}>{user.name}</a>
+                    </p>
+                  </div>
+                </div>
+              )
+            }
           </div>
+        )
+      }
+    </>
 
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
-        </div>
-      </div>
-    </div>
   );
 };
