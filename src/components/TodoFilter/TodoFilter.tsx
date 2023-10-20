7a3365d7e0272b6818080558af58b30520879977
Ir-ra <chapter.ku@ukr.net>
2023-10-20 17:06:24 +0300

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as filterActions } from '../../features/filter';
+import { SearchField } from '../../types/SearchField';
 
 export const TodoFilter: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { status, query } = useAppSelector(state => state.filter);
+
+  const setQuery = (value: string) => dispatch(filterActions.setQuery(value));
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const removeQuery = () => dispatch(filterActions.removeQuery());
+  const handleRemoveQuery = () => {
+    removeQuery();
+  };
+
+  const setStatus = (value: string) => dispatch(filterActions.setStatus(value));
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setStatus(event.target.value);
+  };
+
   return (
     <form
       className="field has-addons"
@@ -8,10 +29,14 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
+          <select
+            data-cy="statusSelect"
+            value={status}
+            onChange={handleSelectChange}
+          >
+            <option value={SearchField.ALL}>All</option>
+            <option value={SearchField.ACTIVE}>Active</option>
+            <option value={SearchField.COMPLETED}>Completed</option>
           </select>
         </span>
       </p>
@@ -22,6 +47,8 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={query}
+          onChange={handleQueryChange}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
@@ -33,6 +60,7 @@ export const TodoFilter: React.FC = () => {
             data-cy="clearSearchButton"
             type="button"
             className="delete"
+            onClick={handleRemoveQuery}
           />
         </span>
       </p>
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,60 @@
-export const actions = { /* put action creators here */};
+type SetQueryAction = { type: 'filter/SET_FILTER_QUERY', payload: string };
+type RemoveQueryAction = {
+  type: 'filter/SET_FILTER_REMOVE_QUERY'
+};
+type SetFilterStatus = { type: 'filter/SET_FILTER_STATUS', payload: string };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type Action = SetQueryAction | RemoveQueryAction | SetFilterStatus;
+
+const setQuery = (query: string): Action => ({
+  type: 'filter/SET_FILTER_QUERY',
+  payload: query,
+});
+
+const removeQuery = (): Action => ({
+  type: 'filter/SET_FILTER_REMOVE_QUERY',
+});
+
+const setStatus = (status: string): Action => ({
+  type: 'filter/SET_FILTER_STATUS',
+  payload: status,
+});
+
+export const actions = { setQuery, removeQuery, setStatus };
+
+type Filter = {
+  query: string;
+  status: string;
+};
+
+const initialFilter: Filter = {
+  query: '',
+  status: 'all',
+};
+
+const filterReducer = (filter: Filter = initialFilter, action: Action) => {
+  switch (action.type) {
+    case 'filter/SET_FILTER_QUERY':
+      return {
+        ...filter,
+        query: action.payload,
+      };
+
+    case 'filter/SET_FILTER_STATUS':
+      return {
+        ...filter,
+        status: action.payload,
+      };
+
+    case 'filter/SET_FILTER_REMOVE_QUERY':
+      return {
+        query: '',
+        status: filter.status,
+      };
+
+    default:
+      return filter;
+  }
 };
 
 export default filterReducer;
