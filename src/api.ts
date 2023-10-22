7eace42a3cf9295c7a160b18032e4c26714951fc
Oleksii-Mishchenko <alex.mishch@gmail.com>
2023-10-23 02:33:48 +0300

    Redux List Of Todos

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
 import { Todo } from './types/Todo';
 import { User } from './types/User';
 
-// eslint-disable-next-line max-len
-const BASE_URL = 'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
+const BASE_URL
+  = 'https://mate-academy.github.io/react_dynamic-list-of-todos/api';
 
 function wait(delay: number): Promise<void> {
   return new Promise(resolve => {
@@ -11,8 +11,7 @@ function wait(delay: number): Promise<void> {
 }
 
 function get<T>(url: string): Promise<T> {
-  // eslint-disable-next-line prefer-template
-  const fullURL = BASE_URL + url + '.json';
+  const fullURL = `${BASE_URL}${url}.json`;
 
   return wait(300)
     .then(() => fetch(fullURL))
