0d79792fe56d186b600aee577aa42e338f7cb3fd
Alina <kozlovska.a.mail@gmail.com>
2023-10-28 23:08:17 +0200

    add task solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,36 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type Filter = { query: string, status: Status };
+type SetQueryAction = { type: 'filter/SET_QUERY', payload: string };
+type ClearQueryAction = { type: 'filter/CLEAR_QUERY' };
+type SetStatusAction = { type: 'filter/SET_STATUS', payload: Status };
+type Action = SetQueryAction | SetStatusAction | ClearQueryAction;
+
+export const actions = {
+  setQuery: (query: string): SetQueryAction => ({
+    type: 'filter/SET_QUERY',
+    payload: query,
+  }),
+  clearQuery: (): ClearQueryAction => ({ type: 'filter/CLEAR_QUERY' }),
+  setStatus: (status: Status): SetStatusAction => ({
+    type: 'filter/SET_STATUS',
+    payload: status,
+  }),
+};
+
+const defaultFilter: Filter = { query: '', status: 'all' };
+
+const filterReducer = (filter: Filter = defaultFilter, action: Action) => {
+  switch (action.type) {
+    case 'filter/SET_QUERY':
+      return { ...filter, query: action.payload };
+    case 'filter/CLEAR_QUERY':
+      return { ...filter, query: '' };
+    case 'filter/SET_STATUS':
+      return { ...filter, status: action.payload };
+    default:
+      return filter;
+  }
 };
 
 export default filterReducer;
