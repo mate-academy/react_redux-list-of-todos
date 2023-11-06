c92bfe950203fc4bf56dcc5d0f3b5537cf760aa7
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-11-06 13:42:30 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-/* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import { useDispatch } from 'react-redux';
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { useAppSelector } from './app/hooks';
+import { actions as TodosActions } from './features/todos';
 
 export const App: React.FC = () => {
+  const [areLoadingTodos, setAreLoadingTodos] = useState(false);
+
+  const dispatch = useDispatch();
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    setAreLoadingTodos(true);
+    getTodos()
+      .then((response) => dispatch(TodosActions.setTodos(response)))
+      .finally(() => setAreLoadingTodos(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +36,19 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {areLoadingTodos ? (
+                <Loader />
+              ) : (
+                <TodoList />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal />
+      )}
     </>
   );
 };
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../../app/hooks';
+import { Status } from '../../types/Status';
+import { actions } from '../../features/filter';
 
-export const TodoFilter: React.FC = () => {
+export const TodoFilter = () => {
+  const dispatch = useDispatch();
+  const filter = useAppSelector(state => state.filter);
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => (
+    dispatch(actions.setStatus(event.target.value as Status))
+  );
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => (
+    dispatch(actions.setQuery(event.target.value))
+  );
+
   return (
-    <form
-      className="field has-addons"
-      onSubmit={event => event.preventDefault()}
-    >
+    <form className="field has-addons">
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
+          <select
+            data-cy="statusSelect"
+            value={filter.status}
+            onChange={handleSelect}
+          >
+            <option value={Status.all}>All</option>
+            <option value={Status.active}>Active</option>
+            <option value={Status.completed}>Completed</option>
           </select>
         </span>
       </p>
@@ -22,18 +37,23 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={filter.query}
+          onChange={handleQueryChange}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
+          {filter && (
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              aria-label="label"
+              onClick={() => dispatch(actions.clearQuery())}
+            />
+          )}
         </span>
       </p>
     </form>
