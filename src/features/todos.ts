3cb3133f5c6aa3da12ade000d891e5914a200e0f
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-11-14 21:13:20 +0200

    feat: redux logic

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,22 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type SetAction = { type: 'todos/SET', payload: Todo[] };
 
-const todosReducer = (): Todo[] => {
-  return [];
+const set = (todos: Todo[]): SetAction => ({
+  type: 'todos/SET',
+  payload: todos,
+});
+
+const todosReducer = (state: Todo[] = [], action: SetAction): Todo[] => {
+  switch (action.type) {
+    case 'todos/SET':
+      return [...state, ...action.payload];
+
+    default:
+      return state;
+  }
 };
+
+export const actions = { set };
 
 export default todosReducer;
