6d24b9b288e983ce7993d1e6d6ed52a4bf07c707
Anton Tsurkan <wesses1999@gmail.com>
2023-11-15 06:46:29 +0200

    add task solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as filterActions } from '../../features/filter';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const { query, status } = useAppSelector(store => store.filter);
+  const dispatch = useAppDispatch();
+
+  const changeQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(filterActions.changeQuery(event.target.value));
+  };
+
+  const changeStatus = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(filterActions.setStatus(event.target.value as Status));
+  };
+
   return (
     <form
       className="field has-addons"
@@ -8,10 +22,14 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
+          <select
+            data-cy="statusSelect"
+            value={status}
+            onChange={changeStatus}
+          >
+            {Object.entries(Status).map(([key, value]) => (
+              <option value={value} key={key}>{key}</option>
+            ))}
           </select>
         </span>
       </p>
@@ -22,19 +40,24 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={query}
+          onChange={changeQuery}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {!!query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => dispatch(filterActions.clearQuery())}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,84 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatus = {
+  type: 'filter/SET_STATUS';
+  payload: Status;
+};
+
+const setStatus = (newStatus: Status): SetStatus => ({
+  type: 'filter/SET_STATUS',
+  payload: newStatus,
+});
+
+type RemoveStatus = {
+  type: 'filter/REMOVE_STATUS';
+};
+
+const removeStatus = (): RemoveStatus => ({
+  type: 'filter/REMOVE_STATUS',
+});
+
+type ChangeQuery = {
+  type: 'filter/CHANGE_QUERY';
+  payload: string;
+};
+
+const changeQuery = (newString: string): ChangeQuery => ({
+  type: 'filter/CHANGE_QUERY',
+  payload: newString,
+});
+
+type ClearQuery = {
+  type: 'filter/CLEAR_QUERY';
+};
+
+const clearQuery = (): ClearQuery => ({
+  type: 'filter/CLEAR_QUERY',
+});
+
+export const actions = {
+  setStatus,
+  removeStatus,
+  changeQuery,
+  clearQuery,
+};
+
+type State = { query: string, status: Status };
+type Action = SetStatus | RemoveStatus | ChangeQuery | ClearQuery;
+
+const defaultState: State = {
+  query: '',
+  status: Status.All,
+};
+
+const filterReducer = (
+  state: State = defaultState,
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'filter/SET_STATUS':
+      return {
+        ...state,
+        status: action.payload,
+      };
+    case 'filter/REMOVE_STATUS':
+      return {
+        ...state,
+        status: Status.All,
+      };
+    case 'filter/CHANGE_QUERY':
+      return {
+        ...state,
+        query: action.payload,
+      };
+    case 'filter/CLEAR_QUERY':
+      return {
+        ...state,
+        query: '',
+      };
+    default:
+      return defaultState;
+  }
 };
 
 export default filterReducer;
