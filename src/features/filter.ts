26379b2d30e6a6e0399c03e30a1324f05a201745
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2023-11-15 12:53:05 +0100

    Solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,50 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
+type SetStatusAction = {
+  type: 'currentStatus/SET',
+  payload: Status,
+};
+
+type SetQueryAction = {
+  type: 'currentQuery/SET',
+  payload: string,
+};
+
+const setStatus = (newStatus: Status): SetStatusAction => ({
+  type: 'currentStatus/SET',
+  payload: newStatus,
+});
+
+const setQuery = (newQuery: string): SetQueryAction => ({
+  type: 'currentQuery/SET',
+  payload: newQuery,
+});
+
+export const actions = { setStatus, setQuery };
+
+type Action = SetStatusAction | SetQueryAction;
+type State = {
+  query: string,
+  status: Status,
+};
+
+const filterReducer = (
+  state: State = {
     query: '',
-    status: 'all',
-  };
+    status: Status.All,
+  },
+  action: Action,
+): State => {
+  switch (action.type) {
+    case 'currentStatus/SET':
+      return { ...state, status: action.payload };
+
+    case 'currentQuery/SET':
+      return { ...state, query: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
