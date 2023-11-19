4a1a762d5629b2a22c24449524e3924c0489ace5
Andrii Khomiak <andr.hamster@gmail.com>
2023-11-19 12:27:27 +0200

    fixes after code review

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -13,61 +13,61 @@ export const TodoModal: React.FC = () => {
 
   useEffect(() => {
     if (currentTodo) {
-      getUser(currentTodo.userId).then(user => {
-        setSelectedUser(user);
-      });
+      getUser(currentTodo.userId).then(setSelectedUser);
     }
   }, []);
 
+  if (!currentTodo) {
+    return null;
+  }
+
   return (
-    currentTodo && (
-      <div className="modal is-active" data-cy="modal">
-        <div className="modal-background" />
+    <div className="modal is-active" data-cy="modal">
+      <div className="modal-background" />
 
-        {!selectedUser ? (
-          <Loader />
-        ) : (
-          <div className="modal-card">
-            <header className="modal-card-head">
-              <div
-                className="modal-card-title has-text-weight-medium"
-                data-cy="modal-header"
-              >
-                Todo #
-                {currentTodo.id}
-              </div>
-
-              <button
-                aria-label="close"
-                type="button"
-                className="delete"
-                data-cy="modal-close"
-                onClick={() => closeModal()}
-              />
-            </header>
-
-            <div className="modal-card-body">
-              <p className="block" data-cy="modal-title">
-                {currentTodo.title}
-              </p>
-
-              <p className="block" data-cy="modal-user">
-                {currentTodo.completed ? (
-                  <strong className="has-text-success">Done</strong>
-                ) : (
-                  <strong className="has-text-danger">Planned</strong>
-                )}
-
-                {' by '}
-
-                <a href={`mailto: ${currentTodo && selectedUser.email}`}>
-                  {currentTodo && selectedUser.name}
-                </a>
-              </p>
+      {!selectedUser ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              Todo #
+              {currentTodo.id}
             </div>
+
+            <button
+              aria-label="close"
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={closeModal}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {currentTodo.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {currentTodo.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
+
+              {' by '}
+
+              <a href={`mailto: ${selectedUser.email}`}>
+                {selectedUser.name}
+              </a>
+            </p>
           </div>
-        )}
-      </div>
-    )
+        </div>
+      )}
+    </div>
   );
 };
