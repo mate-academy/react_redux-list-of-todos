c30fc009f54ae6c2590591475ed4ee7f0ba82832
IhorVerba <ihor.verbivskyi@gmail.com>
2023-11-20 15:04:18 +0200

    solution

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,25 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
-
-const todosReducer = (): Todo[] => {
-  return [];
+type Action = {
+  type: 'todos/SET',
+  payload: Todo[];
 };
 
+const setTodos = (todos: Todo[]): Action => ({
+  type: 'todos/SET',
+  payload: todos,
+});
+
+type State = Todo[];
+
+const todosReducer = (state: State = [], action: Action): State => {
+  switch (action.type) {
+    case 'todos/SET':
+      return [...state, ...action.payload];
+    default:
+      return state;
+  }
+};
+
+export const actions = { setTodos };
 export default todosReducer;
