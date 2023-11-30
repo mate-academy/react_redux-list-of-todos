67fc62cb8f347d110eff45ffdda4224259f51f08
newqou <137334905+newqou@users.noreply.github.com>
2023-11-30 18:18:25 +0200

    dev

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,43 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatus = { type: 'filter/STATUS', payload: Status };
+type SetQuery = { type: 'filter/QUERY', payload: string };
+type ResetQuery = { type: 'filter/RESET_QUERY' };
+
+type State = {
+  query: string;
+  status: Status;
 };
 
+type Actions = SetStatus | SetQuery | ResetQuery;
+
+const setStatus = (status: Status): SetStatus => ({
+  type: 'filter/STATUS',
+  payload: status,
+});
+
+const setQuery = (query: string): SetQuery => ({
+  type: 'filter/QUERY',
+  payload: query,
+});
+
+const resetQuery = (): ResetQuery => ({ type: 'filter/RESET_QUERY' });
+
+const filterReducer = (
+  state: State = { query: '', status: 'all' },
+  action: Actions,
+): State => {
+  switch (action.type) {
+    case 'filter/STATUS':
+      return { ...state, status: action.payload };
+    case 'filter/QUERY':
+      return { ...state, query: action.payload };
+    case 'filter/RESET_QUERY':
+      return { ...state, query: '' };
+    default:
+      return state;
+  }
+};
+
+export const actions = { setStatus, setQuery, resetQuery };
 export default filterReducer;
