2e199eaaae878b2cc827b1e99135250e0f4ff54c
Belia Anton <belja.anton@gmail.com>
2023-12-18 23:15:45 +0200

    add redux list of todos

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,41 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatus = { type: 'status/SET', payload: Status };
+type SetQuery = { type: 'query/SET', payload: string };
+
+type Action = SetStatus | SetQuery;
+
+const setStatus = (value: Status): SetStatus => ({
+  type: 'status/SET',
+  payload: value,
+});
+
+const setQuery = (value: string): SetQuery => ({
+  type: 'query/SET',
+  payload: value,
+});
+
+export const actions = { setStatus, setQuery };
+
+type Filter = {
+  query: string,
+  status: Status,
+};
+
+const initialState: Filter = {
+  query: '',
+  status: 'all' as Status,
+};
+
+const filterReducer = (state = initialState, action: Action) => {
+  switch (action.type) {
+    case 'query/SET':
+      return { ...state, query: action.payload };
+    case 'status/SET':
+      return { ...state, status: action.payload };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
