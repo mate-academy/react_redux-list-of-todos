1b453c7d13f793cc2e5afc6c65b9072c5a0b0a09
Volodymyr Shepel <web.shepel@gmail.com>
2023-12-25 13:42:26 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable max-len */
-import React from 'react';
+import { FC, useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,27 +6,44 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions as todosActions } from './features/todos';
 
-export const App: React.FC = () => {
+export const App: FC = () => {
+  const { todos, isLoading, error } = useAppSelector(state => state.todos);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(todosActions.setLoading(true));
+
+    getTodos()
+      .then(todosFromServer => dispatch(todosActions.setTodos(todosFromServer)))
+      .catch((err) => dispatch(todosActions.setError(err)))
+      .finally(() => dispatch(todosActions.setLoading(false)));
+  }, [dispatch]);
+
+  const hasError = !isLoading && !!error.length;
+  const hasTodoList = !isLoading && !error.length && !!todos.length;
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
             <h1 className="title">Todos:</h1>
-
             <div className="block">
               <TodoFilter />
             </div>
-
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && (<Loader />)}
+              {hasError && (<p>Something went wrong</p>)}
+              {hasTodoList && (<TodoList />)}
             </div>
           </div>
         </div>
       </div>
-
       <TodoModal />
     </>
   );
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,40 +1,78 @@
-import React from 'react';
+import { FC, useEffect, useState } from 'react';
+import cn from 'classnames';
 import { Loader } from '../Loader';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { actions } from '../../features/currentTodo';
 
-export const TodoModal: React.FC = () => {
+export const TodoModal: FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!currentTodo || currentTodo.userId === user?.id) {
+      return;
+    }
+
+    getUser(currentTodo.userId)
+      .then(item => setUser(item))
+      .catch(() => {
+        throw new Error('Something went wrong');
+      });
+  });
+
+  if (!currentTodo) {
+    return null;
+  }
+
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div className={cn('modal', { 'is-active': currentTodo })} data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {currentTodo.userId !== user?.id
+        ? (
+          <Loader />
+        )
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${currentTodo.id}`}
+              </div>
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => dispatch(actions.removeTodo())}
+              >
+                {' '}
+              </button>
+            </header>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">{currentTodo.title}</p>
+              <p className="block" data-cy="modal-user">
+                <strong
+                  className={cn(
+                    {
+                      'has-text-danger': !currentTodo.completed,
+                      'has-text-success': currentTodo.completed,
+                    },
+                  )}
+                >
+                  {currentTodo.completed ? 'Done' : 'Planned'}
+                </strong>
+                {' by '}
+                <a href={user?.email}>{user?.name}</a>
+              </p>
+            </div>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
-        </div>
-      </div>
+        )}
     </div>
   );
 };
