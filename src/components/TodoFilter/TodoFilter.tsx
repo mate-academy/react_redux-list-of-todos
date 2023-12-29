b31292b9755771befafcdcf263ed79d128fc030d
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-30 00:18:35 +0200

    todo-list with redux

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAppDispatch } from '../../app/hooks';
+import { actions as filtersActions } from '../../features/filter';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const [status, setStatus] = useState<string>(Status.ALL);
+  const [query, setQuery] = useState<string>('');
+  const dispatch = useAppDispatch();
+
+  const handleStatusChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setStatus(event.target.value);
+    dispatch(filtersActions.setStatus(event.target.value));
+  };
+
+  const handleQueryChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setQuery(event.target.value);
+    dispatch(filtersActions.setQuery(event.target.value));
+  };
+
+  const handleClear = () => {
+    setQuery('');
+    dispatch(filtersActions.setQuery(''));
+  };
+
   return (
     <form
       className="field has-addons"
@@ -8,10 +34,28 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
+          <select
+            data-cy="statusSelect"
+            value={status}
+            onChange={handleStatusChange}
+          >
+            <option
+              value={Status.ALL}
+            >
+              All
+            </option>
+
+            <option
+              value={Status.ACTIVE}
+            >
+              Active
+            </option>
+
+            <option
+              value={Status.COMPLETED}
+            >
+              Completed
+            </option>
           </select>
         </span>
       </p>
@@ -22,19 +66,24 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          onChange={handleQueryChange}
+          value={query}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              aria-label="clear-search"
+              onClick={handleClear}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,39 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
+import { actions as currentTodoActions } from '../../features/currentTodo';
 
 export const TodoModal: React.FC = () => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const dispatch = useAppDispatch();
+  const todo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const userFromServer = await getUser(todo?.userId || 0);
+
+        setSelectedUser(userFromServer);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [todo]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {isLoading && <Loader />}
 
       <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
-          </div>
+        {isError && !isLoading && !selectedUser && (
+          <p>Cannot find user! Try again!</p>
+        )}
 
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
+        {selectedUser && (
+          <>
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                {`Todo #${todo?.id}`}
+              </div>
 
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                aria-label="close-user"
+                onClick={() => dispatch(currentTodoActions.removeTodo())}
+              />
+            </header>
 
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">{todo?.title}</p>
 
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
-        </div>
+              <p className="block" data-cy="modal-user">
+                {!todo?.completed ? (
+                  <strong className="has-text-danger">
+                    Planned
+                  </strong>
+
+                ) : (
+                  <strong className="has-text-success">
+                    Done
+                  </strong>
+                )}
+                {' by '}
+                <a href={`mailto:${selectedUser.email}`}>
+                  {selectedUser.name}
+                </a>
+              </p>
+            </div>
+          </>
+        )}
+
       </div>
     </div>
   );
