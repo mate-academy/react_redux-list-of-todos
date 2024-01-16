b48ff75e2b58fce5701ab5287b0220546c05cc68
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-16 15:17:43 +0100

    Implement filter action

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,51 @@
-export const actions = { /* put action creators here */};
+const STATUS = 'filter/STATUS';
+const QUERY = 'filter/QUERY';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type Status = 'all' | 'active' | 'completed';
+
+type StatusAction = {
+  type: 'filter/STATUS',
+  payload: Status
 };
+
+type QueryAction = {
+  type: 'filter/QUERY',
+  payload: Status
+};
+
+const setStatus = (status: Status) => ({
+  type: STATUS,
+  payload: status,
+});
+
+const setQuery = (query: string) => ({
+  type: QUERY,
+  payload: query,
+});
+
+type State = {
+  query: string;
+  status: Status;
+};
+
+const initialState: State = {
+  query: '',
+  status: 'all',
+};
+
+type Action = StatusAction | QueryAction;
+
+const filterReducer = (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case STATUS:
+      return { ...state, status: action.payload };
+    case QUERY:
+      return { ...state, query: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const actions = { setQuery, setStatus };
 
 export default filterReducer;
