586137b105c5c1205eba21ccfec30dc238babd7e
Katya Suvorova <katerinasuvorova97@gmail.com>
2024-01-29 20:01:52 +0200

    Solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,40 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUser } from '../../api';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as currentTodoActions } from '../../features/currentTodo';
+import { User } from '../../types/User';
 import { Loader } from '../Loader';
 
 export const TodoModal: React.FC = () => {
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const dispatch = useAppDispatch();
+  const [user, setUser] = useState<User | null>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (currentTodo) {
+      setIsLoading(true);
+      getUser(currentTodo.userId)
+        .then(setUser)
+        .finally(() => setIsLoading(false));
+    }
+  }, [currentTodo]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {
+        isLoading
+          ? <Loader />
+          : (
+            <div className="modal-card">
+              <header className="modal-card-head">
+                <div
+                  className="modal-card-title has-text-weight-medium"
+                  data-cy="modal-header"
+                >
+                  Todo #
+                  {currentTodo?.id}
+                </div>
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <button
+                  type="button"
+                  className="delete"
+                  data-cy="modal-close"
+                  onClick={() => dispatch(currentTodoActions.removeTodo())}
+                />
+              </header>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
-          </div>
+              <div className="modal-card-body">
+                <p className="block" data-cy="modal-title">
+                  {currentTodo?.title}
+                </p>
 
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">fugiat veniam minus</p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
-        </div>
-      </div>
+                <p className="block" data-cy="modal-user">
+                  {
+                    currentTodo?.completed
+                      ? <strong className="has-text-success">Done</strong>
+                      : <strong className="has-text-danger">Planned</strong>
+                  }
+                  {' by '}
+                  <a href={`mailto:${user?.email}`}>{user?.name}</a>
+                </p>
+              </div>
+            </div>
+          )
+      }
     </div>
   );
 };
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,29 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type GetTodoAction = {
+  type: 'todos/GET';
+  payload: Todo[];
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+const getTodos = (value: Todo[]): GetTodoAction => ({
+  type: 'todos/GET',
+  payload: value,
+});
+
+export const actions = { getTodos };
+
+type Action = GetTodoAction;
+
+const todosReducer = (
+  state: Todo[] = [],
+  action: Action,
+): Todo[] => {
+  switch (action.type) {
+    case 'todos/GET':
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
