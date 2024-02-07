cd41040bc80e09f4459f4bb4dbad538c52ea7adb
MrWhiteres <alexandr.leluk.99@gmail.com>
2024-02-07 23:54:53 +0200

    add task solutions

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,10 +1,55 @@
-export const actions = { /* put action creators here */};
+import { Status } from '../types/Status';
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatusAction = {
+  type: 'status/SET';
+  payload: Status;
+};
+
+type SetQueryAction = {
+  type: 'query/SET';
+  payload: string;
+};
+
+const setStatus = (status: Status): SetStatusAction => ({
+  type: 'status/SET',
+  payload: status,
+});
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'query/SET',
+  payload: query,
+});
+
+export const actions = { setStatus, setQuery };
+
+type State = { query: string, status: Status };
+type Action = SetStatusAction | SetQueryAction;
+
+const startFilter: State = {
+  query: '',
+  status: Status.All,
+};
+
+const filterReducer = (
+  state: State = startFilter,
+  action: Action,
+): State => {
+  switch (action.type) {
+    case 'query/SET':
+      return {
+        query: action.payload,
+        status: state.status,
+      };
+
+    case 'status/SET':
+      return {
+        query: state.query,
+        status: action.payload,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
