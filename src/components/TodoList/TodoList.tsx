153cdeb0393cfe6530c2e8e9b288ebf47e75860e
Anton Duchenko <deragandi0@gmail.com>
2024-02-15 08:52:15 +0200

    add message about no todos

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -9,28 +9,34 @@ interface Props {
 
 export const TodoList: React.FC<Props> = ({ todos }) => {
   return (
-    <table className="table is-narrow is-fullwidth">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>
-            <span className="icon">
-              <i className="fas fa-check" />
-            </span>
-          </th>
-          <th>Title</th>
-          <th> </th>
-        </tr>
-      </thead>
+    !todos.length ? (
+      <p className="notification is-warning">
+        There are no todos matching current filter criteria
+      </p>
+    ) : (
+      <table className="table is-narrow is-fullwidth">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>
+              <span className="icon">
+                <i className="fas fa-check" />
+              </span>
+            </th>
+            <th>Title</th>
+            <th> </th>
+          </tr>
+        </thead>
 
-      <tbody>
-        {todos.map(todo => (
-          <TodoItem
-            todo={todo}
-            key={todo.id}
-          />
-        ))}
-      </tbody>
-    </table>
+        <tbody>
+          {todos.map(todo => (
+            <TodoItem
+              todo={todo}
+              key={todo.id}
+            />
+          ))}
+        </tbody>
+      </table>
+    )
   );
 };
