d55d701321cba4ef37b399c9c3e982d5770cdae0
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2024-02-20 10:42:14 +0200

    task done

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,78 @@
-export const actions = {
-  /* put action creators here */
+import { Status } from '../types/Status';
+
+type State = {
+  query: string;
+  status: Status;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+const initialState: State = {
+  query: '',
+  status: 'all',
+};
+
+type SetStatusAction = {
+  type: 'filter/SET_STATUS';
+  payload: Status;
+};
+
+type SetQueryAction = {
+  type: 'filter/SET_QUERY';
+  payload: string;
+};
+
+type ClearQueryAction = {
+  type: 'filter/CLEAR_QUERY';
+  payload: string;
+};
+
+type Action = SetQueryAction | SetStatusAction | ClearQueryAction;
+
+const setStatus = (status: Status): SetStatusAction => ({
+  type: 'filter/SET_STATUS',
+  payload: status,
+});
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter/SET_QUERY',
+  payload: query,
+});
+
+const clearQuery = (): ClearQueryAction => ({
+  type: 'filter/CLEAR_QUERY',
+  payload: '',
+});
+
+export const actions = {
+  setStatus,
+  setQuery,
+  clearQuery,
+};
+
+const filterReducer = (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case 'filter/SET_STATUS':
+      return {
+        ...state,
+        status: action.payload,
+      };
+
+    case 'filter/SET_QUERY':
+      return {
+        ...state,
+        query: action.payload,
+      };
+
+    case 'filter/CLEAR_QUERY':
+      return {
+        ...state,
+        query: '',
+      };
+
+    default:
+      return {
+        ...state,
+      };
+  }
 };
 
 export default filterReducer;
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,25 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type SetTodosAction = {
+  type: 'todo/SET_TODOS';
+  payload: Todo[];
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+const setTodos = (todos: Todo[]): SetTodosAction => ({
+  type: 'todo/SET_TODOS',
+  payload: todos,
+});
+
+export const actions = { setTodos };
+
+const todosReducer = (state: Todo[] = [], action: SetTodosAction): Todo[] => {
+  switch (action.type) {
+    case 'todo/SET_TODOS':
+      return action.payload;
+
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
