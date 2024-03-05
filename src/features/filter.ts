d13129983886133ab7371b83ab298237ae5757c3
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-03-05 10:24:23 +0200

    done

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,47 @@
-export const actions = {
-  /* put action creators here */
+export enum Status {
+  All = 'all',
+  Active = 'active',
+  Completed = 'completed',
+}
+
+type SetQueryAction = { type: 'filter'; payload: string };
+type SetStatusAction = { type: 'sort'; payload: Status };
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter',
+  payload: query,
+});
+
+const setStatus = (status: Status): SetStatusAction => ({
+  type: 'sort',
+  payload: status,
+});
+
+type Action = SetQueryAction | SetStatusAction;
+
+type State = {
+  query: string;
+  status: Status;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+const startValue = {
+  query: '',
+  status: Status.All,
+};
+
+export const actions = { setQuery, setStatus };
+
+const filterReducer = (state: State = startValue, action: Action): State => {
+  switch (action.type) {
+    case 'filter':
+      return { ...state, query: action.payload };
+
+    case 'sort':
+      return { ...state, status: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,21 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type LoadAction = { type: 'load'; payload: Todo[] };
+const loadTodos = (todos: Todo[]): LoadAction => ({
+  type: 'load',
+  payload: todos,
+});
 
-const todosReducer = (): Todo[] => {
-  return [];
+export const actions = { loadTodos };
+
+const todosReducer = (state: Todo[] = [], action: LoadAction): Todo[] => {
+  switch (action.type) {
+    case 'load':
+      return [...state, ...action.payload];
+
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
