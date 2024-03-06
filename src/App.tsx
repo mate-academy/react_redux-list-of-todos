9a0ac199659656ebcb1a82a0956585e077e46327
Roman Kvas <fallenmadara@192.168.1.11>
2024-03-06 12:09:56 +0100

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions } from './features/todos';
+import { getTodos } from './api';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [error, setError] = useState('');
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector(state => state.todos);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  const apiCall = async () => {
+    try {
+      const response = await getTodos();
+
+      dispatch(actions.SetTodos(response));
+    } catch {
+      setError('Oops, something went wrong');
+    }
+  };
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -21,14 +43,14 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {!todos?.length && !error ? <Loader /> : <TodoList />}
+              {error && <p className="notification is-danger">{error}</p>}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
