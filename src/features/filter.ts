90ef544d96381b062d67f3dfbd72bd08cf9b05f9
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2024-03-07 14:24:10 +0200

    done

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,47 @@
-export const actions = {
-  /* put action creators here */
+const setStatus = (status: string): SetStatusAction => ({
+  type: 'filter/STATUS',
+  payload: status,
+});
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter/QUERY',
+  payload: query,
+});
+
+type SetStatusAction = {
+  type: 'filter/STATUS';
+  payload: string;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetQueryAction = {
+  type: 'filter/QUERY';
+  payload: string;
+};
+
+type Action = SetStatusAction | SetQueryAction;
+
+export const actions = {
+  setStatus,
+  setQuery,
+};
+
+const filterReducer = (
+  state = { status: 'all', query: '' },
+  action: Action,
+) => {
+  switch (action.type) {
+    case 'filter/STATUS':
+      return {
+        ...state,
+        status: action.payload,
+      };
+    case 'filter/QUERY':
+      return {
+        ...state,
+        query: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
