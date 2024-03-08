44ac2d9270206949866eb08606bba9d335895372
Semen Sokolovskyi <anori.fjord@gmail.com>
2024-03-08 16:22:20 +0200

    implemented by redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,28 +7,44 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { actions } from './features/todos';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const hasTodos = useAppSelector(state => state.todos.length > 0);
+  const hasSelectedTodo = useAppSelector(state => state.currentTodo !== null);
+
+  useEffect(() => {
+    getTodos().then(todos => dispatch(actions.setTodos(todos)));
+  }, [dispatch]);
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
-            <h1 className="title">Todos:</h1>
-
-            <div className="block">
-              <TodoFilter />
-            </div>
-
-            <div className="block">
+            {!hasTodos ? (
               <Loader />
-              <TodoList />
-            </div>
+            ) : (
+              <>
+                <h1 className="title">Todos:</h1>
+
+                <div className="block">
+                  <TodoFilter />
+                </div>
+
+                <div className="block">
+                  <TodoList />
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {hasSelectedTodo && <TodoModal />}
     </>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,50 @@
-export const actions = {
-  /* put action creators here */
+import { Status } from '../types/Status';
+
+type SetQueryAction = {
+  type: 'filter/SET_QUERY';
+  payload: string;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatusAction = {
+  type: 'filter/SET_STATUS';
+  payload: Status;
+};
+
+type Action = SetQueryAction | SetStatusAction;
+
+type State = {
+  query: string;
+  status: Status;
+};
+
+const initialState: State = {
+  query: '',
+  status: 'all',
+};
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter/SET_QUERY',
+  payload: query,
+});
+
+const setStatus = (status: Status): SetStatusAction => ({
+  type: 'filter/SET_STATUS',
+  payload: status,
+});
+
+export const actions = { setQuery, setStatus };
+
+const filterReducer = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case 'filter/SET_QUERY':
+      return { ...state, query: action.payload };
+
+    case 'filter/SET_STATUS':
+      return { ...state, status: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
