2fe3f24e8334e0c55768ebaef1e068314a64065c
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2024-03-25 00:10:25 +0200

    added redux to list of todos

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import cn from 'classnames';
+import { Todo } from '../../types/Todo';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions as currentTodoActions } from '../../features/currentTodo';
+
+type Props = {
+  todo: Todo;
+};
+
+export const TodoItem: React.FC<Props> = ({ todo }) => {
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(store => store.currentTodo);
+
+  const handleOpenModal = () => {
+    dispatch(currentTodoActions.setTodo(todo));
+  };
+
+  return (
+    <tr data-cy="todo" key={todo.id}>
+      <td className="is-vcentered">{todo.id}</td>
+      <td className="is-vcentered">
+        {todo.completed && (
+          <span className="icon" data-cy="iconCompleted">
+            <i className="fas fa-check" />
+          </span>
+        )}
+      </td>
+
+      <td className="is-vcentered is-expanded">
+        <p
+          className={cn({
+            'has-text-danger': !todo.completed,
+            'has-text-success': todo.completed,
+          })}
+        >
+          {todo.title}
+        </p>
+      </td>
+
+      <td className="has-text-right is-vcentered">
+        <button
+          data-cy="selectButton"
+          className="button"
+          type="button"
+          onClick={handleOpenModal}
+        >
+          <span className="icon">
+            <i
+              className={cn('far', {
+                'fa-eye': currentTodo?.id !== todo.id,
+                'fa-eye-slash': currentTodo?.id === todo.id,
+              })}
+            />
+          </span>
+        </button>
+      </td>
+    </tr>
+  );
+};
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,46 @@
-export const actions = {
-  /* put action creators here */
+/* eslint-disable @typescript-eslint/default-param-last */
+import { Status } from '../types/Status';
+
+type Action = {
+  type: Status;
+  payload: string;
+};
+type Filter = { status: Status; query: string };
+
+const filterActive = (query: string): Action => ({
+  type: 'active',
+  payload: query,
+});
+const filterCompleted = (query: string): Action => ({
+  type: 'completed',
+  payload: query,
+});
+const filterAll = (query: string): Action => ({
+  type: 'all',
+  payload: query,
+});
+
+const applyFilter = (status: Status, query: string): Action => ({
+  type: status,
+  payload: query,
+});
+
+const initialFilter: Filter = { status: 'all', query: '' };
+
+const filterReducer = (filter: Filter = initialFilter, action: Action) => {
+  switch (action.type) {
+    case 'active':
+    case 'completed':
+    case 'all':
+      return {
+        status: action.type,
+        query: action.payload,
+      };
+    default:
+      return filter;
+  }
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
-};
+const actions = { filterActive, filterCompleted, filterAll, applyFilter };
 
-export default filterReducer;
+export { filterReducer, actions };
