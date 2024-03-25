a65f5bee882021707432c0f9647d9263485f8c25
yuron-maker <komanitskiy200320@gmail.com>
2024-03-25 16:29:26 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { TodoList } from './components/TodoList';
+import { Status as SortType } from './types/Status';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { actions as todosActions } from './features/todos';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 
 export const App: React.FC = () => {
+  const todos = useAppSelector(state => state.todos);
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(false);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const filter = useAppSelector(state => state.filter);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(res => dispatch(todosActions.setTodos(res)))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const visibleTodos = useMemo(() => {
+    let filteredTodos = [...todos];
+
+    switch (filter.filter) {
+      case SortType.Active:
+        filteredTodos = filteredTodos.filter(todo => !todo.completed);
+        break;
+      case SortType.Completed:
+        filteredTodos = filteredTodos.filter(todo => todo.completed);
+        break;
+      default:
+        break;
+    }
+
+    if (filter.query) {
+      filteredTodos = filteredTodos.filter(todo =>
+        todo.title
+          .toLocaleLowerCase()
+          .includes(filter.query.toLocaleLowerCase()),
+      );
+    }
+
+    return filteredTodos;
+  }, [filter, todos]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +60,16 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter todos={todos} loading={loading} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading ? <Loader /> : <TodoList todos={visibleTodos} />}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {currentTodo && <TodoModal todo={currentTodo} />}
     </>
   );
 };
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,29 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type SetTodos = {
+  type: 'todos/SET';
+  payload: Todo[];
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+const setTodos = (todos: Todo[]): SetTodos => ({
+  type: 'todos/SET',
+  payload: todos,
+});
+
+export const actions = { setTodos };
+
+type Action = SetTodos;
+
+// eslint-disable-next-line @typescript-eslint/default-param-last
+const todosReducer = (state: Todo[] = [], action: Action): Todo[] => {
+  switch (action.type) {
+    case 'todos/SET': {
+      return action.payload;
+    }
+
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
