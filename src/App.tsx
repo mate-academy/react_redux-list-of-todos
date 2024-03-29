0bd233c3df33622bff50e40e4625b3d19e564a3b
Olena Shapkun <shapkunkh@gmail.com>
2024-03-29 15:36:29 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,27 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { getTodos } from './api';
+import { actions as todosActions } from './features/todos';
 
 export const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(todos => {
+        dispatch(todosActions.setTodos(todos));
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -16,19 +35,18 @@ export const App: React.FC = () => {
           <div className="box">
             <h1 className="title">Todos:</h1>
 
-            <div className="block">
-              <TodoFilter />
-            </div>
+            <div className="block">{!loading && <TodoFilter />}</div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+
+              {!loading && <TodoList />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,29 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
+type SetTodos = {
+  type: 'todos/SET';
+  payload: Todo[];
+};
 
-const todosReducer = (): Todo[] => {
-  return [];
+const setTodos = (value: Todo[]): SetTodos => ({
+  type: 'todos/SET',
+  payload: value,
+});
+
+export const actions = { setTodos };
+
+type State = Todo[] | null;
+type Action = SetTodos;
+
+// eslint-disable-next-line @typescript-eslint/default-param-last
+const todosReducer = (state: State = null, action: Action): State => {
+  switch (action.type) {
+    case 'todos/SET':
+      return action.payload;
+
+    default:
+      return state;
+  }
 };
 
 export default todosReducer;
