3e706dc4ac8ca55be79533dd2f3e93e62213ca18
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-04 12:18:32 +0300

    solution

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,9 +1,29 @@
 import { Todo } from '../types/Todo';
 
-export const actions = {};
-
-const todosReducer = (): Todo[] => {
-  return [];
+type SetTodos = {
+  type: 'todos/SET';
+  payload: Todo[];
 };
+
+const setTodos = (value: Todo[]): SetTodos => ({
+  type: 'todos/SET',
+  payload: value,
+});
+
+type State = Todo[] | null;
+type Action = SetTodos;
+
+// eslint-disable-next-line @typescript-eslint/default-param-last
+const todosReducer = (state: State = null, action: Action): State => {
+  switch (action.type) {
+    case 'todos/SET':
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
+
+export const actions = { setTodos };
 
 export default todosReducer;
