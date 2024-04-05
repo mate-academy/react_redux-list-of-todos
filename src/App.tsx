e87af80577539d6a153020b6fb0dce1bf39ecb17
Yurii Shkrobut <georgebest2409@gmail.com>
2024-04-05 18:01:35 +0300

    currentTodo implemented in Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { Status } from './types/Status';
+import { useAppSelector } from './app/hooks';
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filterStatus, setFilterStatus] = useState<Status>(Status.All);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        throw new Error('Error. Please try again later');
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const filteredTodos: Todo[] = useMemo(() => {
+    let copyTodos = [...todos];
+
+    if (query) {
+      copyTodos = copyTodos.filter(todo =>
+        todo.title.toLowerCase().includes(query.toLowerCase()),
+      );
+    }
+
+    switch (filterStatus) {
+      case Status.Active:
+        return copyTodos.filter(todo => !todo.completed);
+
+      case Status.Completed:
+        return copyTodos.filter(todo => todo.completed);
+
+      default:
+        return copyTodos;
+    }
+  }, [query, filterStatus, todos]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +60,23 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                query={query}
+                setQuery={setQuery}
+                setFilterStatus={setFilterStatus}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+
+              <TodoList todos={filteredTodos} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
--- src/features/currentTodo.ts
+++ src/features/currentTodo.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/default-param-last */
 import { Todo } from '../types/Todo';
 
 // we use string literal as a type to avoid mistype in future
@@ -17,19 +18,23 @@ const setTodo = (todo: Todo): SetTodoAction => ({
   payload: todo,
 });
 
-// These actions will be used in the application
-export const actions = { setTodo, removeTodo };
+//
 
 type State = Todo | null;
 type Action = SetTodoAction | RemoveTodoAction;
 
 const currentTodoReducer = (state: State = null, action: Action): State => {
   switch (action.type) {
-    // Implement all actions here
+    case 'currentTodo/SET':
+      return action.payload;
+
+    case 'currentTodo/REMOVE':
+      return null;
 
     default:
       return state;
   }
 };
 
+export const actions = { setTodo, removeTodo };
 export default currentTodoReducer;
