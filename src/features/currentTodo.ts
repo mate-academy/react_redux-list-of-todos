38b2cc7ce33e109b550462ba7c8a742de5282de8
Alla Serhiienko <allasergienko706@gmail.com>
2024-04-13 21:07:28 +0200

    Update src/features/currentTodo.ts
    Co-authored-by: Vadym Voiedilo <108701488+vadiimvooo@users.noreply.github.com>

--- src/features/currentTodo.ts
+++ src/features/currentTodo.ts
@@ -24,8 +24,10 @@ const currentTodoReducer = (state: State = null, action: Action): State => {
   switch (action.type) {
     case 'currentTodo/SET':
       return action.payload;
+      
     case 'currentTodo/REMOVE':
       return null;
+      
     default:
       return state;
   }
