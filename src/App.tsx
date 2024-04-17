ae5ee2b6f8f102c628585e235bee765febb470d5
Antonina Klishch <tonya91t@gmail.com>
2024-04-17 13:59:24 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
-import { TodoModal } from './components/TodoModal';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { getTodos } from './api';
+import { actions as actionsTodos } from './features/todos';
 import { Loader } from './components/Loader';
+import { TodoModal } from './components/TodoModal';
 
 export const App: React.FC = () => {
+  const { loading, todos } = useAppSelector(state => state.todos);
+  const { currentTodo } = useAppSelector(state => state.currentTodo);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(actionsTodos.setLoadig(true));
+
+    getTodos()
+      .then(todosFromServer => dispatch(actionsTodos.setTodos(todosFromServer)))
+      .catch(error => dispatch(actionsTodos.setError(error)))
+      .finally(() => dispatch(actionsTodos.setLoadig(false)));
+  }, [dispatch]);
+
   return (
     <>
       <div className="section">
         <div className="container">
           <div className="box">
             <h1 className="title">Todos:</h1>
-
             <div className="block">
               <TodoFilter />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              {!loading && todos.length > 0 && <TodoList />}
             </div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,4 @@
+import { Status } from './types/Status';
 import { Todo } from './types/Todo';
 import { User } from './types/User';
 
@@ -23,3 +24,31 @@ function get<T>(url: string): Promise<T> {
 export const getTodos = () => get<Todo[]>('/todos');
 
 export const getUser = (userId: number) => get<User>(`/users/${userId}`);
+
+export const normalizeText = (text: string) => {
+  return text.toLowerCase().trim();
+};
+
+export const getTodosFilter = (
+  listTodos: Todo[],
+  filterBy: Status,
+  query: string,
+) => {
+  let filterTodos = [...listTodos];
+
+  if (query) {
+    filterTodos = filterTodos.filter(todo =>
+      normalizeText(todo.title).includes(normalizeText(query)),
+    );
+  }
+
+  switch (filterBy) {
+    case Status.Active:
+      return filterTodos.filter(todo => !todo.completed);
+    case Status.Completed:
+      return filterTodos.filter(todo => todo.completed);
+    case Status.All:
+    default:
+      return filterTodos;
+  }
+};
