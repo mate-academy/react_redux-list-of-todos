eb0fcfdf59b635ff8e1954359dedd19b27231a98
Andrzej Pietkiewicz <pietan@me.com>
2024-04-23 23:40:15 +0200

    remove comments

--- src/features/currentTodo.ts
+++ src/features/currentTodo.ts
@@ -1,16 +1,13 @@
 import { Todo } from '../types/Todo';
 
-// we use string literal as a type to avoid mistype in future
 type RemoveTodoAction = {
   type: 'currentTodo/REMOVE' };
 
-// payload is a typical name for an action data
 type SetTodoAction = {
   type: 'currentTodo/SET';
   payload: Todo;
 };
 
-// Action creator return type protect us from a mistype
 const removeTodo = (): RemoveTodoAction => ({ type: 'currentTodo/REMOVE' });
 
 const setTodo = (todo: Todo): SetTodoAction => ({
@@ -35,7 +32,6 @@ const currentTodoReducer = (
   }
 };
 
-// These actions will be used in the application
 export const actions = { setTodo, removeTodo };
 
 export default currentTodoReducer;
