d5de105609928dc2dafcc594ca5239f7cef8387e
Andrzej Pietkiewicz <pietan@me.com>
2024-04-24 23:51:13 +0200

    fix modal loading

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -41,40 +41,41 @@ export const TodoModal: React.FC = () => {
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {isLoading && <Loader />}
+      {isLoading
+        ? <Loader />
+        : (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                Todo
+                {' '}
+                {currentTodo.id}
+              </div>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo
-            {' '}
-            {currentTodo.id}
-          </div>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={() => dispatch(actions.removeTodo())}
+              />
+            </header>
 
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            type="button"
-            className="delete"
-            data-cy="modal-close"
-            onClick={() => dispatch(actions.removeTodo())}
-          />
-        </header>
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">{currentTodo.title}</p>
 
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">{currentTodo.title}</p>
-
-          <p className="block" data-cy="modal-user">
-            <strong className={cn({
-              'has-text-success': currentTodo,
-              'has-text-danger': !currentTodo,
-            })}
-            >
-              {currentTodo.completed ? 'Done' : 'Planned'}
-            </strong>
-            {currentTodo.completed
+              <p className="block" data-cy="modal-user">
+                <strong className={cn({
+                  'has-text-success': currentTodo,
+                  'has-text-danger': !currentTodo,
+                })}
+                >
+                  {currentTodo.completed ? 'Done' : 'Planned'}
+                </strong>
+                {currentTodo.completed
               && (
                 <>
 
@@ -83,9 +84,10 @@ export const TodoModal: React.FC = () => {
                 </>
               )}
 
-          </p>
-        </div>
-      </div>
+              </p>
+            </div>
+          </div>
+        )}
     </div>
   );
 };
