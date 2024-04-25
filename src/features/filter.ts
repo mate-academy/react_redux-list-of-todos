6c35e1fa62f44a09dddc79b6cdb8522c1f43b5a5
shikoryak <oksanashykoriak@gmail.com>
2024-04-25 11:31:57 +0300

    add solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,37 @@
-export const actions = {
-  /* put action creators here */
+export type FilterState = {
+  query: string;
+  status: string;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetFilterAction = {
+  type: 'filter/SET';
+  payload: FilterState;
+};
+
+const setFilter = (filter: FilterState): SetFilterAction => ({
+  type: 'filter/SET',
+  payload: filter,
+});
+
+export const actions = { setFilter };
+
+type Action = SetFilterAction;
+const defaultFilter = {
+  query: '',
+  status: 'all',
+};
+
+const filterReducer = (
+  // eslint-disable-next-line @typescript-eslint/default-param-last
+  state: FilterState = defaultFilter,
+  action: Action,
+): FilterState => {
+  switch (action.type) {
+    case 'filter/SET':
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
