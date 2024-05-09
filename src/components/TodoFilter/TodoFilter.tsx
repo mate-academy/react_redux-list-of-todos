b73c7b8e8e585a2089b1b5fb2c36f81c7a3a6606
Max Petrashishin <rikogjdefolt@gmail.com>
2024-05-09 10:35:48 +0300

    solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { actions } from '../../features/filter';
 
 export const TodoFilter: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const filter = useAppSelector(state => state.filter);
+
   return (
     <form
       className="field has-addons"
@@ -8,7 +13,11 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            value={filter.status}
+            onChange={e => dispatch(actions.setStatus(e.target.value))}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -22,6 +31,8 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={filter.query}
+          onChange={e => dispatch(actions.setQuery(e.target.value))}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
@@ -33,6 +44,7 @@ export const TodoFilter: React.FC = () => {
             data-cy="clearSearchButton"
             type="button"
             className="delete"
+            onClick={() => dispatch(actions.setQuery(''))}
           />
         </span>
       </p>
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,57 @@
-export const actions = {
-  /* put action creators here */
+type SetQueryAction = {
+  type: 'filter/QUERYSET';
+  payload: string;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetStatusAction = {
+  type: 'filter/STATUSSET';
+  payload: string;
+};
+
+type Action = SetQueryAction | SetStatusAction;
+
+const setQuery = (query: string): SetQueryAction => ({
+  type: 'filter/QUERYSET',
+  payload: query,
+});
+
+const setStatus = (status: string): SetStatusAction => ({
+  type: 'filter/STATUSSET',
+  payload: status,
+});
+
+export const actions = {
+  setQuery,
+  setStatus,
+};
+
+type FilterData = {
+  query: string;
+  status: string;
+};
+
+const filterData: FilterData = {
+  query: '',
+  status: 'all',
+};
+
+type State = FilterData;
+
+const filterReducer = (
+  // eslint-disable-next-line @typescript-eslint/default-param-last
+  state: State = filterData,
+  action: Action,
+): FilterData => {
+  switch (action.type) {
+    case 'filter/QUERYSET':
+      return { ...state, query: action.payload };
+
+    case 'filter/STATUSSET':
+      return { ...state, status: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
