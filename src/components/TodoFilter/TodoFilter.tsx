e6410df5042cd20c813e81e6411cb06bd72045b1
Ivan Gaponenko <gimkiev@gmail.com>
2024-05-19 16:15:21 +0200

    add task solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -5,10 +5,12 @@ import React from 'react';
 export const TodoFilter: React.FC = () => {
   const dispatch = useAppDispatch();
   const filter = useAppSelector(state => state.filter);
+  const { query } = useAppSelector(state => state.filter);
 
   const onSelectChange = (status: string) =>
     dispatch(actions.setFilter({ ...filter, status }));
 
+  // eslint-disable-next-line @typescript-eslint/no-shadow
   const onQueryChange = (query: string) =>
     dispatch(actions.setFilter({ ...filter, query }));
 
@@ -44,14 +46,19 @@ export const TodoFilter: React.FC = () => {
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {query.length > 0 && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              onClick={() =>
+                dispatch(actions.setFilter({ ...filter, query: '' }))
+              }
+              className="delete"
+            />
+          </span>
+        )}
       </p>
     </form>
   );
