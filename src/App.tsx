1bbb2c977363e467ea70c9cebbaaf7794d7a95e8
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-05-21 03:00:10 +0300

    solved the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
 
 import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { getTodos } from './api';
+import { actions as todoActions } from './features/todos';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then(response => {
+        dispatch(todoActions.set(response));
+      })
+      .finally(() => setIsLoading(false));
+  }, [dispatch]);
+
   return (
     <>
       <div className="section">
@@ -21,14 +40,14 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              <TodoList isLoading={isLoading} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && <TodoModal />}
     </>
   );
 };
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,53 @@
-export const actions = {
-  /* put action creators here */
+import { Status } from '../types/Status';
+
+type QueryAction = {
+  type: 'filter/setQuery';
+  payload: string;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type StatusAction = {
+  type: 'filter/setStatus';
+  payload: Status;
+};
+
+const setQuery = (query: string): QueryAction => ({
+  type: 'filter/setQuery',
+  payload: query,
+});
+
+const setStatus = (status: Status): StatusAction => ({
+  type: 'filter/setStatus',
+  payload: status,
+});
+
+export const actions = {
+  setStatus,
+  setQuery,
+};
+
+type State = {
+  query: string;
+  status: string;
+};
+type Action = QueryAction | StatusAction;
+
+const initialState: State = {
+  query: '',
+  status: Status.All,
+};
+
+// eslint-disable-next-line
+const filterReducer = (state: State = initialState, action: Action): State => {
+  switch (action.type) {
+    case 'filter/setQuery':
+      return { ...state, query: action.payload };
+
+    case 'filter/setStatus':
+      return { ...state, status: action.payload };
+
+    default:
+      return state;
+  }
 };
 
 export default filterReducer;
