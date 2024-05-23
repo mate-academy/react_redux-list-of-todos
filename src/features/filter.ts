86021c74c4c077fa4d4d2a8aa65da7f2e42d6602
SerjDSA <serjdsa7@gmail.com>
2024-05-23 06:47:42 +0300

    add task solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,50 @@
-export const actions = {
-  /* put action creators here */
+import { Status } from '../types/Status';
+
+type SetStatus = {
+  type: 'filter/SET_STATUS';
+  payload: string;
 };
 
-const filterReducer = () => {
-  return {
-    query: '',
-    status: 'all',
-  };
+type SetQuery = {
+  type: 'filter/SET_QUERY';
+  payload: string;
+};
+
+const setStatus = (value: string): SetStatus => ({
+  type: 'filter/SET_STATUS',
+  payload: value,
+});
+
+const setQuery = (value: string): SetQuery => ({
+  type: 'filter/SET_QUERY',
+  payload: value,
+});
+
+export const actions = { setQuery, setStatus };
+
+type Action = SetStatus | SetQuery;
+
+type State = {
+  query: string;
+  status: string;
+};
+
+const initialState: State = {
+  query: '',
+  status: Status.All,
+};
+
+// eslint-disable-next-line @typescript-eslint/default-param-last
+const filterReducer = (state: State = initialState, action: Action) => {
+  switch (action.type) {
+    case 'filter/SET_QUERY':
+      return { ...state, query: action.payload };
+    case 'filter/SET_STATUS':
+      return { ...state, status: action.payload };
+
+    default:
+      return initialState;
+  }
 };
 
 export default filterReducer;
