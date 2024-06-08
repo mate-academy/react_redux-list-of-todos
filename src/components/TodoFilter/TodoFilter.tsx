e1e8448bd5c91adc38a45f9d5c1eb5e095b630e0
katya havriuk <0303gavkat@gmail.com>
2024-06-07 18:55:42 -0600

    task solved

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { Status } from '../../types/Status';
+import { actions as actionsFilter } from '../../features/filter';
 
-export const TodoFilter: React.FC = () => {
+export const TodoFilter = () => {
+  const dispatch = useAppDispatch();
+  const { status, query } = useAppSelector(state => state.filter);
+
+  const setStatus = (newStatus: Status) =>
+    dispatch(actionsFilter.setStatus(newStatus));
+  const setQuery = (newQuery: string) =>
+    dispatch(actionsFilter.setQuery(newQuery));
+
+  const onChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedFilter = e.target.value as Status;
+
+    setStatus(selectedFilter);
+  };
+
+  const onChangeField = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const text = e.target.value;
+
+    setQuery(text);
+  };
+
   return (
-    <form
-      className="field has-addons"
-      onSubmit={event => event.preventDefault()}
-    >
+    <form className="field has-addons">
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            value={status}
+            onChange={onChangeSelect}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -22,19 +45,24 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={query}
+          onChange={onChangeField}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {!!query.length && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => setQuery('')}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
--- src/features/currentTodo.ts
+++ src/features/currentTodo.ts
@@ -23,10 +23,18 @@ export const actions = { setTodo, removeTodo };
 type State = Todo | null;
 type Action = SetTodoAction | RemoveTodoAction;
 
-const currentTodoReducer = (state: State = null, action: Action): State => {
-  switch (action.type) {
-    // Implement all actions here
+const initialState: State = null;
 
+const currentTodoReducer = (
+  // eslint-disable-next-line @typescript-eslint/default-param-last
+  state: State = initialState,
+  action: Action,
+): State => {
+  switch (action.type) {
+    case 'currentTodo/SET':
+      return action.payload;
+    case 'currentTodo/REMOVE':
+      return null;
     default:
       return state;
   }
