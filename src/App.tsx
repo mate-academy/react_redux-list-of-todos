c0e0af1541515284585a0a441cfab5cfb3170ccc
dmytro-voronin <d.voronin.owen@gmail.com>
2024-06-24 13:02:53 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useMemo, useState } from 'react';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { todosSlice } from './features/todos';
+import { Todo } from './types/Todo';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState('');
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const todos = useAppSelector(state => state.todos);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const filter = useAppSelector(state => state.filter);
+  const dispatch = useAppDispatch();
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then((data: Todo[]) => dispatch(todosSlice.actions.setTodos(data)))
+      .finally(() => setIsLoading(false));
+  }, [dispatch]);
+
+  const preparedTodos = useMemo(() => {
+    let sortedTodos = [];
+
+    switch (filter) {
+      case 'active':
+        sortedTodos = todos.filter(todo => !todo.completed);
+        break;
+
+      case 'completed':
+        sortedTodos = todos.filter(todo => todo.completed);
+        break;
+
+      default:
+        sortedTodos = todos;
+    }
+
+    return sortedTodos.filter(todo =>
+      todo.title.toLowerCase().includes(query.toLowerCase().trim()),
+    );
+  }, [filter, query, todos]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter setQuery={setQuery} />
+            </div>
+
+            <div className="block">
+              {isLoading && <Loader />}
+              <TodoList todos={preparedTodos} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <TodoModal />
-  </>
-);
+      {currentTodo && <TodoModal />}
+    </>
+  );
+};
