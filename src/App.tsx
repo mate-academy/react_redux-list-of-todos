c0e1155c8ee4981fbdf60dfdfcb8834cc778f740
Alina Ovod <alina.ipatova98@gmail.com>
2024-06-27 11:33:12 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { useEffect, useMemo, useState } from 'react';
+import { todosSlice } from './features/todos';
+import { getTodos } from './api';
+import { Status } from './types/Status';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const [loading, setLoading] = useState(true);
+  const todos = useAppSelector(state => state.todos);
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+  const filter = useAppSelector(state => state.filter);
+  const dispatch = useAppDispatch();
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    setLoading(true);
+    getTodos()
+      .then(data => dispatch(todosSlice.actions.setTodos(data)))
+      .finally(() => setLoading(false));
+  }, [dispatch]);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  const filteredTodos = useMemo(() => {
+    return todos
+      .filter(todo => {
+        switch (filter.status) {
+          case Status.Active:
+            return !todo.completed;
+          case Status.Completed:
+            return todo.completed;
+          default:
+            return true;
+        }
+      })
+      .filter(todo =>
+        todo.title.toLowerCase().includes(filter.query.toLowerCase()),
+      );
+  }, [todos, filter]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {loading && <Loader />}
+              {!loading && <TodoList todos={filteredTodos} />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {selectedTodo && <TodoModal />}
+    </>
+  );
+};
