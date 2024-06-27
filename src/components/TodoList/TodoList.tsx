8f8ed99e9f3fd32b3155d266549e3253f48f2781
Yuliia <kotyuliya24@gmail.com>
2024-06-27 13:07:02 -0400

    solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -4,9 +4,9 @@ import classNames from 'classnames';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { currentTodoSlice } from '../../features/currentTodo';
 
-interface Props {
+type Props = {
   todos: Todo[];
-}
+};
 
 export const TodoList: React.FC<Props> = ({ todos }) => {
   const dispatch = useAppDispatch();
@@ -35,51 +35,57 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
         </thead>
 
         <tbody>
-          {todos.map(todo => (
-            <tr
-              key={todo.id}
-              data-cy="todo"
-              className={classNames({
-                'has-background-info-light': todo.id % 2 === 0,
-              })}
-            >
-              <td className="is-vcentered">{todo.id}</td>
-              <td className="is-vcentered">
-                {todo.completed && (
-                  <span className="icon" data-cy="iconCompleted">
-                    <i className="fas fa-check" />
-                  </span>
-                )}
-              </td>
-              <td className="is-vcentered is-expanded">
-                <p
-                  className={
-                    todo.completed ? 'has-text-success' : 'has-text-danger'
-                  }
-                >
-                  {todo.title}
-                </p>
-              </td>
-              <td className="has-text-right is-vcentered">
-                <button
-                  data-cy="selectButton"
-                  className="button"
-                  type="button"
-                  onClick={() => {
-                    dispatch(currentTodoSlice.actions.setTodo(todo));
-                  }}
-                >
-                  <span className="icon">
-                    <i
-                      className={
-                        selectedTodo ? 'far fa-eye-slash' : 'far fa-eye'
-                      }
-                    />
-                  </span>
-                </button>
-              </td>
-            </tr>
-          ))}
+          {todos.map(todo => {
+            const { id, title, completed } = todo;
+
+            return (
+              <tr
+                key={id}
+                data-cy="todo"
+                className={classNames({
+                  'has-background-info-light': id % 2 === 0,
+                })}
+              >
+                <td className="is-vcentered">{id}</td>
+                <td className="is-vcentered">
+                  {completed && (
+                    <span className="icon" data-cy="iconCompleted">
+                      <i className="fas fa-check" />
+                    </span>
+                  )}
+                </td>
+                <td className="is-vcentered is-expanded">
+                  <p
+                    className={classNames({
+                      'has-text-danger': completed === false,
+                      'has-text-success': completed === true,
+                    })}
+                  >
+                    {title}
+                  </p>
+                </td>
+                <td className="has-text-right is-vcentered">
+                  <button
+                    data-cy="selectButton"
+                    className="button"
+                    type="button"
+                    onClick={() => {
+                      dispatch(currentTodoSlice.actions.setTodo(todo));
+                    }}
+                  >
+                    <span className="icon">
+                      <i
+                        className={classNames('far', {
+                          'fa-eye-slash': selectedTodo?.id === id,
+                          'fa-eye': selectedTodo?.id !== id,
+                        })}
+                      />
+                    </span>
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
