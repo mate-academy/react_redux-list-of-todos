976daa5db1e5e76f1896ba966819238ecfc787bc
Anastasiia Hurska <nastyagurska@gmail.com>
2024-07-02 12:57:54 +0300

    case sensitivity fixed

--- src/components/TableBody/TableBody.tsx
+++ src/components/TableBody/TableBody.tsx
@@ -1,39 +1,11 @@
-import { useAppSelector } from '../../app/hooks';
-import { useMemo } from 'react';
 import { TableRow } from '../TableRow';
+import { Todo } from '../../types/Todo';
 
-enum Status {
-  completed = 'completed',
-  active = 'active',
-}
+type Props = {
+  visibleTodos: Todo[];
+};
 
-export const TableBody: React.FC = () => {
-  const todos = useAppSelector(state => state.todos);
-  const { status, query } = useAppSelector(state => state.filter);
-
-  const visibleTodos = useMemo(() => {
-    let todosProcessed = [...todos];
-
-    switch (status) {
-      case Status.completed:
-        todosProcessed = todosProcessed.filter(todo => !todo.completed);
-        break;
-      case Status.active:
-        todosProcessed = todosProcessed.filter(todo => todo.completed);
-        break;
-      default:
-        break;
-    }
-
-    if (query) {
-      todosProcessed = todosProcessed.filter(todo =>
-        todo.title.includes(query),
-      );
-    }
-
-    return todosProcessed;
-  }, [status, query]);
-
+export const TableBody: React.FC<Props> = ({ visibleTodos }) => {
   if (!visibleTodos.length) {
     return (
       <p className="notification is-warning">
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,13 +1,44 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useMemo } from 'react';
 import { TableHead } from '../TableHead';
 import { TableBody } from '../TableBody';
+import { useAppSelector } from '../../app/hooks';
+
+enum Status {
+  completed = 'completed',
+  active = 'active',
+}
 
 export const TodoList: React.FC = () => {
+  const todos = useAppSelector(state => state.todos);
+  const { status, query } = useAppSelector(state => state.filter);
+
+  const visibleTodos = useMemo(() => {
+    let todosProcessed = [...todos];
+
+    switch (status) {
+      case Status.completed:
+        todosProcessed = todosProcessed.filter(todo => !todo.completed);
+        break;
+      case Status.active:
+        todosProcessed = todosProcessed.filter(todo => todo.completed);
+        break;
+      default:
+        break;
+    }
+
+    if (query) {
+      todosProcessed = todosProcessed.filter(todo =>
+        todo.title.toLocaleLowerCase().includes(query.toLocaleUpperCase()),
+      );
+    }
+
+    return todosProcessed;
+  }, [status, query]);
   return (
     <table className="table is-narrow is-fullwidth">
-      <TableHead />
-      <TableBody />
+      {!!visibleTodos.length && <TableHead />}
+      <TableBody visibleTodos={visibleTodos} />
     </table>
   );
 };
