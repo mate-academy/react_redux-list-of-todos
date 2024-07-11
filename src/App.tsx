f653956235dee6d362442dd8772441f6f20276ca
kyrylomanko <kyrylo.manko@gmail.com>
2024-07-11 15:26:02 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useSelector } from 'react-redux';
+import { Todo } from './types/Todo';
+import { TodoFilter } from './components/TodoFilter/TodoFilter';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodoModal } from './components/TodoModal/TodoModal';
+import { Loader } from './components/Loader/Loader';
+import { useAppSelector } from './app/hooks';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const currentTodo = useSelector((state: Todo) => state.currentTodoReducer);
+  const todos = useAppSelector(state => state.todosReducer);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {!todos.length && <Loader />}
+              <TodoList />
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {currentTodo && <TodoModal />}
+    </>
+  );
+};
