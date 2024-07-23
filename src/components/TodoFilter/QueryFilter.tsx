4aee953d761c0f12f94719178158eb4475bdd890
Makhmud <bmm0065@gmail.com>
2024-07-23 14:17:45 +0300

    add task solution

---
+++ src/components/TodoFilter/QueryFilter.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useAppDispatch } from '../../app/hooks';
+import { setQueryFilter } from '../../features/filter';
+
+export const QueryFilter = () => {
+  const dispatch = useAppDispatch();
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    dispatch(setQueryFilter(inputValue));
+  }, [inputValue]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setInputValue(value);
+  };
+
+  const handleClear = () => {
+    setInputValue('');
+  };
+
+  return (
+    <p className="control is-expanded has-icons-left has-icons-right">
+      <input
+        value={inputValue}
+        data-cy="searchInput"
+        type="text"
+        className="input"
+        placeholder="Search..."
+        onChange={handleChange}
+      />
+      <span className="icon is-left">
+        <i className="fas fa-magnifying-glass" />
+      </span>
+
+      {inputValue && (
+        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+          <button
+            data-cy="clearSearchButton"
+            type="button"
+            className="delete"
+            onClick={handleClear}
+          />
+        </span>
+      )}
+    </p>
+  );
+};
