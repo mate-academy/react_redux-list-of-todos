5fe4e0103365b74c9323acd9ad88da06365a973d
Katya <evil.666.buny@gmail.com>
2024-07-29 10:25:23 +0300

    solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { Status } from '../types/Status';
 
 const initialState = {
   query: '',
   status: 'all',
 };
 
-export const filterSlice = createSlice({
+export interface FilterState {
+  query: string;
+  status: Status;
+}
+
+const filterSlice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setStatusFilter: (state, action: PayloadAction<Status>) => {
+      return {
+        ...state,
+        status: action.payload,
+      };
+    },
+    setQueryFilter: (state, action: PayloadAction<string>) => {
+      return {
+        ...state,
+        query: action.payload,
+      };
+    },
+  },
 });
+
+export const { setStatusFilter, setQueryFilter } = filterSlice.actions;
+export default filterSlice.reducer;
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,8 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { Todo } from '../types/Todo';
 
-export const todosSlice = createSlice({
+export interface TodosState {
+  data: Todo[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: TodosState = {
+  data: [],
+  loading: false,
+  error: null,
+};
+
+const todosSlice = createSlice({
   name: 'todos',
-  initialState: [] as Todo[],
-  reducers: {},
+  initialState,
+  reducers: {
+    initialTodos: (state, action: PayloadAction<Todo[]>) => {
+      return { ...state, data: action.payload };
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      return { ...state, loading: action.payload };
+    },
+    setError: (state, action: PayloadAction<string | null>) => {
+      return { ...state, error: action.payload };
+    },
+  },
 });
+
+export const { initialTodos, setLoading, setError } = todosSlice.actions;
+export default todosSlice.reducer;
