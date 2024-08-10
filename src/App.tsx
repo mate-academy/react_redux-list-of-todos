6b4c749aaa577518bd0647aeea396bc890ec05b1
Stanislav Kushnir <florian.of.lorch@gmail.com>
2024-08-10 16:14:30 +0300

    made react redux list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { useEffect, useMemo, useState } from 'react';
+import { getTodos } from './api';
+import { addTodos } from './features/todos';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const todos = useAppSelector(state => state.todos.value);
+  const currTodo = useAppSelector(state => state.currTodo.value);
+  const filterStatus = useAppSelector(state => state.filterTodos.select);
+  const filterQuery = useAppSelector(state => state.filterTodos.query);
+  const dispatch = useAppDispatch();
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  useEffect(() => {
+    getTodos()
+      .then(todosFromServer => dispatch(addTodos(todosFromServer)))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const filterTodos = useMemo(() => {
+    let newTodos = [...todos];
+
+    if (filterStatus) {
+      newTodos = newTodos.filter(item => {
+        switch (filterStatus) {
+          case 'active':
+            return item.completed === false;
+          case 'completed':
+            return item.completed;
+          case 'all':
+          default:
+            return true;
+        }
+      });
+    }
+
+    if (filterQuery) {
+      newTodos = newTodos.filter(item => {
+        return item.title.toLowerCase().includes(filterQuery.toLowerCase());
+      });
+    }
+
+    return newTodos;
+  }, [todos, filterStatus, filterQuery]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {isLoading ? <Loader /> : <TodoList todos={filterTodos} />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {currTodo && <TodoModal />}
+    </>
+  );
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,42 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { deselectTodo } from '../../features/currentTodo';
+import { getUser } from '../../api';
+import { User } from '../../types/User';
 
 export const TodoModal: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const currTodo = useAppSelector(state => state.currTodo.value);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (currTodo) {
+      getUser(currTodo.userId).then(setUser);
+    }
+  }, [currTodo]);
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
+      {!user ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${currTodo?.id}`}
+            </div>
 
-      <Loader />
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => {
+                dispatch(deselectTodo());
+              }}
+            />
+          </header>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {currTodo?.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              <strong
+                className={
+                  currTodo?.completed ? 'has-text-success' : 'has-text-danger'
+                }
+              >
+                {currTodo?.completed ? 'Done' : 'Planned'}
+              </strong>
+
+              {' by '}
+
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">
-            fugiat veniam minus
-          </p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
