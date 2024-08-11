b7724a7f95e0024fe64f49702014c1aa32f3a1c1
Sergii Serdiuk <162016576+seriuksergii@users.noreply.github.com>
2024-08-11 13:29:26 +0300

    add solution task react_redux-list-of-todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useState } from 'react';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { todosSlice } from './features/todos';
+import { Todo } from './types/Todo';
+import { getFilteredTodos } from './utils/getFilteredTodos';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const [loading, setLoading] = useState(false);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector(state => state.todos);
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+  const filter = useAppSelector(state => state.filter);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  const filteredTodos = getFilteredTodos(todos, filter);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then((data: Todo[]) => {
+        dispatch(todosSlice.actions.setTodos(data));
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {loading && <Loader />}
+              {!loading && <TodoList todos={filteredTodos} />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {selectedTodo && <TodoModal />}
+    </>
+  );
+};
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { currentTodoSlice } from '../features/currentTodo';
+import { todosSlice } from '../features/todos';
+import { filterSlice } from '../features/filter';
 
-const rootReducer = combineSlices();
+const rootReducer = combineSlices(currentTodoSlice, todosSlice, filterSlice);
 
 export const store = configureStore({
   reducer: rootReducer,
 });
-
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
