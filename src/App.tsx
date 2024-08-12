d4044e6f20101d393bd48181b65f40c9f9d646fb
Stanislav <stanislavandr@gmail.com>
2024-08-12 09:11:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useState } from 'react';
+import { getTodos } from './api';
+import { todosSlice } from './features/todos';
+import { Todo } from './types/Todo';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { Status } from './types/Status';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const dispatch = useAppDispatch();
+  const todos = useAppSelector(state => state.todos);
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+  const filter = useAppSelector(state => state.filter);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  function getFilteredTodos(
+  todos: Todo[],
+  filter: { status: Status; query: string },
+) {
+  let filteredTodos = [...todos];
+  const normalizedQuery = filter.query.trim().toLowerCase();
+
+  switch (filter.status) {
+    case 'active':
+      filteredTodos = todos.filter(todo => !todo.completed);
+      break;
+
+    case 'completed':
+      filteredTodos = todos.filter(todo => todo.completed);
+      break;
+
+    default:
+      break;
+  }
+
+  if (normalizedQuery) {
+    return filteredTodos.filter(todo =>
+      todo.title.toLowerCase().includes(normalizedQuery),
+    );
+  }
+
+  return filteredTodos;
+  }
+
+  const filteredTodos = getFilteredTodos(todos, filter);
+
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTodos()
+      .then((data: Todo[]) => {
+        dispatch(todosSlice.actions.setTodos(data));
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+            <div className="block">
+              {isLoading && <Loader />}
+              {!isLoading && <TodoList todos={filteredTodos} />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <TodoModal />
-  </>
-);
+      {selectedTodo && <TodoModal />}
+    </>
+  );
+};
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, Slice, createSlice } from '@reduxjs/toolkit';
+import { Status } from '../types/Status';
 
 const initialState = {
   query: '',
   status: 'all',
 };
 
-export const filterSlice = createSlice({
+export const filterSlice: Slice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setFilter: (state, { payload }: PayloadAction<Status>) => {
+      return { ...state, status: payload };
+    },
+    setQuery: (state, { payload }: PayloadAction<string>) => {
+      return { ...state, query: payload };
+    },
+  },
 });
+
+
