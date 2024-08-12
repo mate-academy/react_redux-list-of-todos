236d52cbb68d07b366796d70ccb4c3d0f47383d5
Illia Kocherovets <illiakocherovets@gmail.com>
2024-08-12 20:46:16 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useState } from 'react';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { todosSlice } from './features/todos';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+export const App = () => {
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const dispatch = useAppDispatch();
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getTodos()
+      .then((data: Todo[]) => dispatch(todosSlice.actions.setTodos(data)))
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">{loading ? <Loader /> : <TodoList />}</div>
           </div>
         </div>
       </div>
-    </div>
-
-    <TodoModal />
-  </>
-);
+      {currentTodo && <TodoModal />}
+    </>
+  );
+};
