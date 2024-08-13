c7656e00e4b891a468f5baaae49a705697dc221b
Les Dyman <lesdyman@gmail.com>
2024-08-13 14:49:42 +0200

    add initial solution

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,8 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, Slice } from '@reduxjs/toolkit';
 import { Todo } from '../types/Todo';
+import { AppDispatch } from '../app/store';
+import { getTodos as fetchTodosFromApi } from '../api';
+import { setLoading } from './loader';
 
-export const todosSlice = createSlice({
+export const todosSlice: Slice<Todo[]> = createSlice({
   name: 'todos',
   initialState: [] as Todo[],
-  reducers: {},
+  reducers: {
+    loadTodos: (_state, action: PayloadAction<Todo[]>) => action.payload,
+  },
 });
+
+export const { loadTodos } = todosSlice.actions;
+export default todosSlice.reducer;
+
+export const fetchTodos = () => async (dispatch: AppDispatch) => {
+  try {
+    dispatch(setLoading(true));
+    const todos = await fetchTodosFromApi();
+
+    dispatch(loadTodos(todos));
+  } catch (error) {
+    throw new Error('error occured while loading todos');
+  } finally {
+    dispatch(setLoading(false));
+  }
+};
