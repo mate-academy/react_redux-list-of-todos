4129a8e9eb7017857b8e344ed94bb404bf195884
Teollan <serhiikofficial@gmail.com>
2024-09-03 21:02:05 +0300

    initial commit

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import { ChangeEvent } from 'react';
+import { Filter } from '../../enums/Filter';
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { setStatus, setQuery } from '../../features/filter/filterSlice';
 
-export const TodoFilter: React.FC = () => {
+export const TodoFilter = () => {
+  const dispatch = useAppDispatch();
+
+  const { status, query } = useAppSelector(state => state.filter);
+
+  const handleFilterChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setStatus(event.target.value as Filter));
+  };
+
+  const handleQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setQuery(event.target.value));
+  };
+
+  const handleQueryClear = () => {
+    dispatch(setQuery(''));
+  };
+
   return (
-    <form
-      className="field has-addons"
-      onSubmit={event => event.preventDefault()}
-    >
+    <form className="field has-addons">
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
+          <select
+            onChange={handleFilterChange}
+            data-cy="statusSelect"
+            value={status}
+          >
+            <option value={Filter.All}>All</option>
+            <option value={Filter.Active}>Active</option>
+            <option value={Filter.Completed}>Completed</option>
           </select>
         </span>
       </p>
 
       <p className="control is-expanded has-icons-left has-icons-right">
         <input
+          onChange={handleQueryChange}
+          value={query}
           data-cy="searchInput"
           type="text"
           className="input"
@@ -28,12 +50,14 @@ export const TodoFilter: React.FC = () => {
         </span>
 
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
+          {query && (
+            <button
+              onClick={handleQueryClear}
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+            />
+          )}
         </span>
       </p>
     </form>
