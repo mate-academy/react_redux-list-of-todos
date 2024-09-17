37251f55a28f6ec1569f5905184b8a09249ae014
Den <clixqqk@gmail.com>
2024-09-17 17:18:42 +0300

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ export const App = () => {
       });
   }, [dispatch]);
 
-
   return (
     <>
       <div className="section">
@@ -46,5 +45,5 @@ export const App = () => {
 
       {currentTodo && <TodoModal />}
     </>
-  )
+  );
 };
