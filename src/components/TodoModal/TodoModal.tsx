0e12f73c532a312038149b84224b19f9a5b6703f
Vladimir Kolesnikov <9858713@gmail.com>
2024-09-20 12:27:02 +0300

    refactoring

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -28,44 +28,46 @@ export const TodoModal: React.FC = () => {
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {loading && <Loader />}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              {`Todo #${id}`}
+            </div>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            {`Todo #${id}`}
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              onClick={() => {
+                dispatch(currentTodoSlice.actions.removeCurrentTodo());
+              }}
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
+              {' by '}
+              <a href={`mailto:${currentUser?.email}`}>{currentUser?.name}</a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            onClick={() => {
-              dispatch(currentTodoSlice.actions.removeCurrentTodo());
-            }}
-            type="button"
-            className="delete"
-            data-cy="modal-close"
-          />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">
-            {title}
-          </p>
-
-          <p className="block" data-cy="modal-user">
-            {completed ? (
-              <strong className="has-text-success">Done</strong>
-            ) : (
-              <strong className="has-text-danger">Planned</strong>
-            )}
-            {' by '}
-            <a href={`mailto:${currentUser?.email}`}>{currentUser?.name}</a>
-          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
