97a38b9bd0988f82f7bf9c92a5a5fe689a8b895d
Bohdan Ivankov <bodanich600@gmail.com>
2024-09-23 19:02:08 +0100

    remove redundant comments

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -90,39 +90,3 @@ export const TodoList: React.FC<Props> = ({
     </>
   );
 };
-
-{
-  /* <tr data-cy="todo">
-<td className="is-vcentered">1</td>
-<td className="is-vcentered"> </td>
-
-<td className="is-vcentered is-expanded">
-  <p className="has-text-danger">delectus aut autem</p>
-</td>
-
-<td className="has-text-right is-vcentered">
-  <button data-cy="selectButton" className="button" type="button">
-    <span className="icon">
-      <i className="far fa-eye" />
-    </span>
-  </button>
-</td>
-</tr>
-
-<tr data-cy="todo" className="has-background-info-light">
-<td className="is-vcentered">3</td>
-<td className="is-vcentered"> </td>
-
-<td className="is-vcentered is-expanded">
-  <p className="has-text-danger">fugiat veniam minus</p>
-</td>
-
-<td className="has-text-right is-vcentered">
-  <button data-cy="selectButton" className="button" type="button">
-    <span className="icon">
-      <i className="far fa-eye-slash" />
-    </span>
-  </button>
-</td>
-</tr> */
-}
