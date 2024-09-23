364c73800004634e73e4f199a90e10db800569bd
Oksana Palahecha <oksanapalahecha@gmail.com>
2024-09-23 20:08:08 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { getTodos } from './api';
+import { setTodos } from './features/todos';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App: React.FC = () => {
+  const todos = useAppSelector(state => state.todos);
+  const filterStatus = useAppSelector(state => state.filter.status);
+  const query = useAppSelector(state => state.filter.query);
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(getTodo => dispatch(setTodos(getTodo)))
+      .finally(() => setIsLoading(false));
+  }, [dispatch]);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  const filteredTodos = todos
+    .filter(todo => {
+      if (filterStatus === 'active') {
+        return !todo.completed;
+      }
+
+      if (filterStatus === 'completed') {
+        return todo.completed;
+      }
+
+      return true;
+    })
+    .filter(todo => todo.title.toLowerCase().includes(query.toLowerCase()));
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todos={filteredTodos}
+                  selectedTodoId={currentTodo ? currentTodo.id : null}
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+        {currentTodo && <TodoModal />}
+      </div>
+    </>
+  );
+};
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,16 @@
-import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import {
+  combineReducers as combineSlices,
+  configureStore,
+} from '@reduxjs/toolkit';
+import { filterSlice } from '../features/filter';
+import { todosSlice } from '../features/todos';
+import { currentTodoSlice } from '../features/currentTodo';
 
-const rootReducer = combineSlices();
+const rootReducer = combineSlices({
+  filter: filterSlice.reducer,
+  todos: todosSlice.reducer,
+  currentTodo: currentTodoSlice.reducer,
+});
 
 export const store = configureStore({
   reducer: rootReducer,
