6c484f8780576dd641761bc554016e863a2051ae
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-09-24 13:23:10 +0200

    fixed table display in case of no todos found with applied queries

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -49,71 +49,73 @@ export const TodoList: React.FC = () => {
         </p>
       )}
 
-      <table className="table is-narrow is-fullwidth">
-        <thead>
-          <tr>
-            <th>#</th>
+      {!noVisibleTodos && (
+        <table className="table is-narrow is-fullwidth">
+          <thead>
+            <tr>
+              <th>#</th>
 
-            <th>
-              <span className="icon">
-                <i className="fas fa-check" />
-              </span>
-            </th>
+              <th>
+                <span className="icon">
+                  <i className="fas fa-check" />
+                </span>
+              </th>
 
-            <th>Title</th>
-            <th> </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {visibleTodos.map(todo => (
-            <tr
-              data-cy="todo"
-              className={classNames('', {
-                'has-background-info-light': todo.id === currentTodo?.id,
-              })}
-            >
-              <td className="is-vcentered">{todo.id}</td>
-              <td className="is-vcentered">
-                {todo.completed && (
-                  <span className="icon" data-cy="iconCompleted">
-                    <i className="fas fa-check" />
-                  </span>
-                )}
-              </td>
-
-              <td className="is-vcentered is-expanded">
-                <p
-                  className={classNames('', {
-                    'has-text-danger': !todo.completed,
-                    'has-text-success': todo.completed,
-                  })}
-                >
-                  {todo.title}
-                </p>
-              </td>
-
-              <td className="has-text-right is-vcentered">
-                <button
-                  data-cy="selectButton"
-                  className="button"
-                  type="button"
-                  onClick={() => handleSetTodo(todo)}
-                >
-                  <span className="icon">
-                    <i
-                      className={classNames('far', {
-                        'fa-eye': todo.id !== currentTodo?.id,
-                        'fa-eye-slash': todo.id === currentTodo?.id,
-                      })}
-                    />
-                  </span>
-                </button>
-              </td>
+              <th>Title</th>
+              <th> </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {visibleTodos.map(todo => (
+              <tr
+                data-cy="todo"
+                className={classNames('', {
+                  'has-background-info-light': todo.id === currentTodo?.id,
+                })}
+              >
+                <td className="is-vcentered">{todo.id}</td>
+                <td className="is-vcentered">
+                  {todo.completed && (
+                    <span className="icon" data-cy="iconCompleted">
+                      <i className="fas fa-check" />
+                    </span>
+                  )}
+                </td>
+
+                <td className="is-vcentered is-expanded">
+                  <p
+                    className={classNames('', {
+                      'has-text-danger': !todo.completed,
+                      'has-text-success': todo.completed,
+                    })}
+                  >
+                    {todo.title}
+                  </p>
+                </td>
+
+                <td className="has-text-right is-vcentered">
+                  <button
+                    data-cy="selectButton"
+                    className="button"
+                    type="button"
+                    onClick={() => handleSetTodo(todo)}
+                  >
+                    <span className="icon">
+                      <i
+                        className={classNames('far', {
+                          'fa-eye': todo.id !== currentTodo?.id,
+                          'fa-eye-slash': todo.id === currentTodo?.id,
+                        })}
+                      />
+                    </span>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
