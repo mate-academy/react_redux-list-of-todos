d84b94b7bd71e1eef634e1e81d6726f364d1705f
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-09-24 12:54:39 +0200

    used common variable + useMemo instead of state to store todos, delete redundant comments

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -46,40 +46,42 @@ export const TodoModal: React.FC = () => {
 
       {isLoading ? (
         <Loader />
-      ) : currentUser ? (
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <div
-              className="modal-card-title has-text-weight-medium"
-              data-cy="modal-header"
-            >
-              Todo #{currentTodo.id}
+      ) : (
+        currentUser && (
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <div
+                className="modal-card-title has-text-weight-medium"
+                data-cy="modal-header"
+              >
+                Todo #{currentTodo.id}
+              </div>
+              <button
+                type="button"
+                className="delete"
+                data-cy="modal-close"
+                onClick={handleCloseTodo}
+              />
+            </header>
+
+            <div className="modal-card-body">
+              <p className="block" data-cy="modal-title">
+                {currentTodo.title}
+              </p>
+
+              <p className="block" data-cy="modal-user">
+                {!currentTodo.completed ? (
+                  <strong className="has-text-danger">Planned</strong>
+                ) : (
+                  <strong className="has-text-success">Done</strong>
+                )}
+                {' by '}
+                <a href={`mailto:${currentUser.email}`}>{currentUser.name}</a>
+              </p>
             </div>
-            <button
-              type="button"
-              className="delete"
-              data-cy="modal-close"
-              onClick={handleCloseTodo}
-            />
-          </header>
-
-          <div className="modal-card-body">
-            <p className="block" data-cy="modal-title">
-              {currentTodo.title}
-            </p>
-
-            <p className="block" data-cy="modal-user">
-              {!currentTodo.completed ? (
-                <strong className="has-text-danger">Planned</strong>
-              ) : (
-                <strong className="has-text-success">Done</strong>
-              )}
-              {' by '}
-              <a href={`mailto:${currentUser.email}`}>{currentUser.name}</a>
-            </p>
           </div>
-        </div>
-      ) : null}
+        )
+      )}
     </div>
   );
 };
