127c319e60c81f553e8552c41398f93f5425523b
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2024-09-25 20:18:40 +0200

    add task solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,15 @@
-import { combineSlices, configureStore } from '@reduxjs/toolkit';
-
-const rootReducer = combineSlices();
+import { configureStore } from '@reduxjs/toolkit';
+import currentTodo from '../features/currentTodo';
+import filterSlice from '../features/filter';
+import todosSlice from '../features/todos';
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    currentTodo: currentTodo,
+    filter: filterSlice,
+    todos: todosSlice,
+  },
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,8 +1,22 @@
+/* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 import { Todo } from '../types/Todo';
+import { RootState } from '../app/store';
+
+const initialState = {
+  todos: [] as Todo[],
+};
 
 export const todosSlice = createSlice({
   name: 'todos',
-  initialState: [] as Todo[],
-  reducers: {},
+  initialState: initialState,
+  reducers: {
+    setTodos: (state, action) => {
+      state.todos = action.payload;
+    },
+  },
 });
+
+export const { setTodos } = todosSlice.actions;
+export default todosSlice.reducer;
+export const selectTodos = (state: RootState) => state.todos.todos;
