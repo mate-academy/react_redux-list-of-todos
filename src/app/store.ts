da06065e3a0f07bf174ec2a0b682d4dadcd98180
piyopiyo0 <tetyana0204@gmail.com>
2024-09-27 15:21:16 +0300

    solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,13 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
 
-const rootReducer = combineSlices();
+import { currentTodoSlice } from '../features/currentTodo';
+import { todosSlice } from '../features/todos';
+import { filterSlice } from '../features/filter';
+
+const rootReducer = combineSlices(currentTodoSlice, todosSlice, filterSlice);
 
 export const store = configureStore({
   reducer: rootReducer,
 });
-
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
