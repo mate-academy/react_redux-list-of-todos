285c4263189af5e14865f349bcc178fc6b30019d
Oleksii Kislukhin <kislukhin.lesha@gmail.com>
2024-09-29 21:40:34 +0300

    add solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,3 +1,37 @@
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import { Status } from '../types/Status';
+
+// interface State {
+//   query: string;
+//   status: Status;
+// }
+
+// const initialState: State = {
+//   query: '',
+//   status: 'all',
+// };
+
+// export const filterSlice = createSlice({
+//   name: 'filter',
+//   initialState,
+//   reducers: {
+//     changeStatus(state: State, { payload }: PayloadAction<Status>) {
+//       return {
+//         ...state,
+//         status: payload,
+//       };
+//     },
+//     changeQuary(state: State, { payload }: PayloadAction<string>) {
+//       return {
+//         ...state,
+//         query: payload,
+//       };
+//     },
+//   },
+// });
+
+// export const { changeQuary, changeStatus } = filterSlice.actions;
+
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
