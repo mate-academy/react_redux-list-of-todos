eb332c59a745b08656dd859a5b00db0d71165695
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2024-10-02 20:28:34 +0300

    fix

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -16,69 +16,79 @@ export const TodoList: React.FC = () => {
   const dispatch = useDispatch();
 
   return (
-    <table className="table is-narrow is-fullwidth">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>
-            <span className="icon">
-              <i className="fas fa-check" />
-            </span>
-          </th>
-          <th>Title</th>
-          <th> </th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {filteredTodos.map(todo => {
-          return (
-            <tr
-              data-cy="todo"
-              className={classnames({
-                'has-background-info-light': selectedTodo,
-              })}
-              key={todo.id}
-            >
-              <td className="is-vcentered">{todo.id}</td>
-              <td className="is-vcentered">
-                {todo.completed && (
-                  <span className="icon" data-cy="iconCompleted">
-                    <i className="fas fa-check" />
-                  </span>
-                )}
-              </td>
-              <td className="is-vcentered is-expanded">
-                <p
-                  className={classnames({
-                    'has-text-danger': todo.completed === false,
-                    'has-text-success': todo.completed === true,
-                  })}
-                >
-                  {todo.title}
-                </p>
-              </td>
-              <td className="has-text-right is-vcentered">
-                <button
-                  data-cy="selectButton"
-                  className="button"
-                  type="button"
-                  onClick={() => dispatch(setCurrentTodo(todo))}
-                >
-                  <span className="icon">
-                    <i
-                      className={classnames('far', {
-                        'fa-eye-slash': selectedTodo?.id === todo.id,
-                        'fa-eye': selectedTodo?.id !== todo.id,
-                      })}
-                    />
-                  </span>
-                </button>
-              </td>
+    <>
+      {!filteredTodos.length ? (
+        <p className="notification is-warning">
+          There are no todos matching current filter criteria
+        </p>
+      ) : (
+        <table className="table is-narrow is-fullwidth">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>
+                <span className="icon">
+                  <i className="fas fa-check" />
+                </span>
+              </th>
+              <th>Title</th>
+              <th> </th>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          </thead>
+
+          <tbody>
+            {filteredTodos.map(todo => {
+              const { completed, id, title } = todo;
+
+              return (
+                <tr
+                  data-cy="todo"
+                  className={classnames({
+                    'has-background-info-light': selectedTodo,
+                  })}
+                  key={id}
+                >
+                  <td className="is-vcentered">{id}</td>
+                  <td className="is-vcentered">
+                    {completed && (
+                      <span className="icon" data-cy="iconCompleted">
+                        <i className="fas fa-check" />
+                      </span>
+                    )}
+                  </td>
+                  <td className="is-vcentered is-expanded">
+                    <p
+                      className={classnames({
+                        'has-text-danger': !completed,
+                        'has-text-success': completed,
+                      })}
+                    >
+                      {title}
+                    </p>
+                  </td>
+                  <td className="has-text-right is-vcentered">
+                    <button
+                      data-cy="selectButton"
+                      className="button"
+                      type="button"
+                      onClick={() => dispatch(setCurrentTodo(todo))}
+                    >
+                      <span className="icon">
+                        <i
+                          className={classnames('far', {
+                            'fa-eye-slash': selectedTodo?.id === id,
+                            'fa-eye': selectedTodo?.id !== id,
+                          })}
+                        />
+                      </span>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 };
