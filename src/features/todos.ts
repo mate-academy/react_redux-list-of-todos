80325b9a5ed00e82d7e5dfb6438d55ba5b0c5dfd
Olena Nikishyna <lyashenko.ol.yur@gmail.com>
2024-10-07 01:15:46 +0300

    added task solution

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -1,8 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSelector, createSlice } from '@reduxjs/toolkit';
 import { Todo } from '../types/Todo';
+import { RootState } from '../app/store';
 
 export const todosSlice = createSlice({
   name: 'todos',
   initialState: [] as Todo[],
-  reducers: {},
+  reducers: {
+    set: (_, action) => {
+      return action.payload;
+    },
+  },
 });
+
+const todos = (state: RootState) => state.todos;
+
+export const todosSelector = createSelector([todos], value => {
+  return value;
+});
+
+export const { actions } = todosSlice;
