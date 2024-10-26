4ecd5912df4c7ef7eff07647f4b31142a969e013
Valentyn Radobenko <valikradobenko@gmail.com>
2024-10-26 21:56:05 +0300

    add solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { useAppSelector } from '../../app/store';
+import { actions, filterSelector } from '../../features/filter';
+import { useDispatch } from 'react-redux';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const filter = useAppSelector(filterSelector);
+  const dispatch = useDispatch();
+
   return (
     <form
-      className="field has-addons"
       onSubmit={event => event.preventDefault()}
+      className="field has-addons"
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
-            <option value="all">All</option>
-            <option value="active">Active</option>
-            <option value="completed">Completed</option>
+          <select
+            value={filter.status}
+            onChange={e =>
+              dispatch(actions.setStatusFilter(e.target.value as Status))
+            }
+            data-cy="statusSelect"
+          >
+            <option value={Status.All}>All</option>
+            <option value={Status.Active}>Active</option>
+            <option value={Status.Completed}>Completed</option>
           </select>
         </span>
       </p>
@@ -22,19 +35,24 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={filter.query}
+          onChange={e => dispatch(actions.setSearchQuery(e.target.value))}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {filter.query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => dispatch(actions.resetFilters())}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
