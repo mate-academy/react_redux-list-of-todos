d7cb90de3463891a14592400f29f325dd525c99f
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-10-29 16:51:55 +0200

    solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,42 +1,76 @@
-import React from 'react';
-import { Loader } from '../Loader';
+import { useEffect, useState } from 'react';
 
-export const TodoModal: React.FC = () => {
+import { Loader } from '../Loader';
+import { User } from '../../types/User';
+import { getUser } from '../../api';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import { useDispatch } from 'react-redux';
+import { currentTodoSlice } from '../../features/currentTodo';
+
+export const TodoModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const dispatch = useDispatch();
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    if (selectedTodo) {
+      setIsLoading(true);
+      getUser(selectedTodo?.userId)
+        .then(setUser)
+        .finally(() => setIsLoading(false));
+    }
+  }, [selectedTodo]);
+
+  const handleModalClose = () => {
+    dispatch(currentTodoSlice.actions.selectTodo(null));
+  };
+
   return (
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              Todo #{selectedTodo?.id}
+            </div>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={handleModalClose}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {selectedTodo?.title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {/* <strong className="has-text-success">Done</strong> */}
+              {selectedTodo?.completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
+
+              {' by '}
+
+              <a href={`mailto:${user?.email}`}>{user?.name}</a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">
-            fugiat veniam minus
-          </p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
---
+++ src/utils/getFilteredTodos.ts
@@ -0,0 +1,36 @@
+import { Todo } from '../types/Todo';
+import { Status } from '../types/Status';
+
+type Params = {
+  query: string;
+  status: Status;
+};
+
+export const getFilteredTodos = (todos: Todo[], { query, status }: Params) => {
+  let filteredTodos = [...todos];
+
+  filteredTodos = filteredTodos.filter(todo => {
+    switch (status) {
+      case 'active':
+        return !todo.completed;
+
+      case 'completed':
+        return todo.completed;
+
+      default:
+        return todo;
+    }
+  });
+
+  const normilizedQuery = query.toLowerCase().trim();
+
+  if (normilizedQuery) {
+    filteredTodos = filteredTodos.filter(({ title }) => {
+      const normilizedTitle = title.toLowerCase();
+
+      return normilizedTitle.includes(normilizedQuery);
+    });
+  }
+
+  return filteredTodos;
+};
