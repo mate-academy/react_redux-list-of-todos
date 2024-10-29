6d6c8a1bd73d214603b4e9ea4e26037ff91530bf
Mariia <maria.gula0307@gmail.com>
2024-10-29 21:47:17 +0200

    solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,8 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { currentTodoSlice, todosSlice } from '../features';
+import { filterSlice } from '../features';
 
-const rootReducer = combineSlices();
+const rootReducer = combineSlices(todosSlice, currentTodoSlice, filterSlice);
 
 export const store = configureStore({
   reducer: rootReducer,
---
+++ src/utils/getFilteredTodos.ts
@@ -0,0 +1,26 @@
+import { Status, Todo } from '../types';
+
+interface Props {
+  status: Status;
+  query: string;
+}
+
+export function getFilteredTodos(todos: Todo[], { query, status }: Props) {
+  let filteredTodos = [...todos];
+
+  if (status !== 'all') {
+    filteredTodos = filteredTodos.filter(({ completed }) =>
+      status === 'active' ? !completed : completed,
+    );
+  }
+
+  const normalizedTitleFilter = query.toLocaleLowerCase().trim();
+
+  if (normalizedTitleFilter) {
+    filteredTodos = filteredTodos.filter(({ title }) =>
+      title.toLocaleLowerCase().includes(normalizedTitleFilter),
+    );
+  }
+
+  return filteredTodos;
+}
