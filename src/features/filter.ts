e91cd370e582acacdcd8c8f8e40dfcbc7d5fccc6
Olya <olyavidzi97@gmail.com>
2024-10-31 13:40:39 +0200

    solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../app/store';
 
 const initialState = {
   query: '',
@@ -8,5 +9,24 @@ const initialState = {
 export const filterSlice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setQuery: (state, action: PayloadAction<string>) => {
+      return {
+        ...state,
+        query: action.payload,
+      };
+    },
+    setFilter: (state, action: PayloadAction<string>) => {
+      return {
+        ...state,
+        status: action.payload,
+      };
+    },
+  },
 });
+
+export const { setQuery, setFilter } = filterSlice.actions;
+export const selectStatus = (state: RootState) => state.filter.status;
+export const selectQuery = (state: RootState) => state.filter.query;
+
+export default filterSlice.reducer;
