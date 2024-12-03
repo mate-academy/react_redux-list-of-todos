04201cf4c315fcb7185269b84103e475fbebbe21
daradanilova <lemma1102@gmail.com>
2024-12-03 14:30:21 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { FC, useEffect, useState } from 'react';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { getTodos } from './api';
+import { getFilteredData } from './utils/getFilteredData';
+import { useAppDispatch, useAppSelector } from './app/hook';
+import { todosSlice } from './features/todos';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App: FC = () => {
+  const [loading, setLoading] = useState(false);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const dispatch = useAppDispatch();
+  const openedTodo = useAppSelector(state => state.currentTodo);
+  const todos = useAppSelector(state => state.todos);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  useEffect(() => {
+    setLoading(true);
+
+    getTodos()
+      .then(res => dispatch(todosSlice.actions.setTodos(res)))
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+
+  const query = useAppSelector(state => state.filter.query);
+  const selectedCondition = useAppSelector(state => state.filter.status);
+
+  const filteredData = getFilteredData(todos, { query, selectedCondition });
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {loading && <Loader />}
+
+              {!loading && todos.length > 0 && (
+                <TodoList todos={filteredData} />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {openedTodo && <TodoModal />}
+    </>
+  );
+};
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { SelectedType } from '../types/SelectedType';
 
 const initialState = {
   query: '',
-  status: 'all',
+  status: SelectedType.All,
 };
 
 export const filterSlice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setFilter: (state, action: PayloadAction<SelectedType>) => {
+      return { ...state, status: action.payload };
+    },
+    setQuery: (state, action: PayloadAction<string>) => {
+      return { ...state, query: action.payload };
+    },
+    clearQuery: state => {
+      return { ...state, query: '' };
+    },
+  },
 });
+
+export const { setFilter, setQuery, clearQuery } = filterSlice.actions;
+export default filterSlice.reducer;
+
+//зберігають запит і статус //query and status
