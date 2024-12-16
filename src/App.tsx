c16e7461d372e3dddfcf6f3440918a15f0239727
Denys Khuda <dan.network.17@gmail.com>
2024-12-16 16:12:42 +0200

    add redux list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from './app/hooks';
+import { actions as todosActions } from './features/todos';
+import { getTodos } from './api';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useDispatch();
+  const { isLoading } = useAppSelector(state => state.todos);
+  const currentTodo = useAppSelector(state => state.currentTodo);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    dispatch(todosActions.setLoading(true));
+    getTodos()
+      .then(todosFromServer => {
+        dispatch(todosActions.setTodos(todosFromServer));
+      })
+      .catch(() => {
+        dispatch(todosActions.setError('Error'));
+      })
+      .finally(() => {
+        dispatch(todosActions.setLoading(false));
+      });
+  }, [dispatch]);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">{isLoading ? <Loader /> : <TodoList />}</div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {currentTodo && <TodoModal currentTodo={currentTodo} />}
+    </>
+  );
+};
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TodoStatusFilter } from '../constants/StatusFilter';
 
-const initialState = {
-  query: '',
-  status: 'all',
+type FilterState = {
+  query: string;
+  status: TodoStatusFilter;
 };
 
-export const filterSlice = createSlice({
+const initialState: FilterState = {
+  query: '',
+  status: TodoStatusFilter.All,
+};
+
+const filterSlice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setQuery: (state, action: PayloadAction<string>) => {
+      state.query = action.payload;
+    },
+    clearQuery: state => {
+      state.query = '';
+    },
+    setStatus: (state, action: PayloadAction<TodoStatusFilter>) => {
+      state.status = action.payload;
+    },
+  },
 });
+
+export default filterSlice.reducer;
+export const { actions } = filterSlice;
