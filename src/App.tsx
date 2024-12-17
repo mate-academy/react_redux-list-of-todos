e1d010bc68b5a1cbe87268169c90989c6c9b2a2a
Sophia <sophia13gal@gmail.com>
2024-12-17 13:34:13 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useAppSelector } from './app/hooks';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const { loading } = useAppSelector(state => state.todos);
+  const currentTodo = useAppSelector(state => state.currentTodo) || null;
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {loading && <Loader />}
+              <TodoList />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <TodoModal />
-  </>
-);
+      {currentTodo && <TodoModal />}
+    </>
+  );
+};
