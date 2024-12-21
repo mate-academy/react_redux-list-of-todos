f6168fa88803179f09a4c0022a1467518370a734
an-marryKyslenko <olih.a@icloud.com>
2024-12-21 13:50:49 +0100

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useState } from 'react';
+import { todosSlice } from './features/todos';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(state => state.currentTodo);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(result => {
+        dispatch(todosSlice.actions.storeTodos(result));
+      })
+      .catch(() => {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 3000);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {isLoading ? (
+                <Loader />
+              ) : isError ? (
+                <div>Something went wrong!</div>
+              ) : (
+                <TodoList />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {currentTodo && <TodoModal />}
+    </>
+  );
+};
--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { filterSlice } from '../../features/filter';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const { query, status } = useAppSelector(state => state.filter);
+  const dispatch = useAppDispatch();
+
+  const handleStatus = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value as Status;
+
+    dispatch(filterSlice.actions.status(value));
+  };
+
+  const handleQuery = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(filterSlice.actions.query(e.target.value));
+  };
+
+  const clearQuery = () => {
+    dispatch(filterSlice.actions.clear());
+  };
+
   return (
     <form
       className="field has-addons"
@@ -8,7 +28,7 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select data-cy="statusSelect" onChange={handleStatus} value={status}>
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -22,19 +42,24 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={query}
+          onChange={handleQuery}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={clearQuery}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
