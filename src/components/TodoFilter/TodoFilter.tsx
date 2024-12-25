3e37c30e294690201e81755eb47b5e6a3ddcae9f
IShamkii <fanmetalheavy@gmail.com>
2024-12-25 15:19:22 +0200

    add task solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { filterSlice } from '../../features/filter';
+import { Status } from '../../types/Status';
 
 export const TodoFilter: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const query = useAppSelector(state => state.filter.query);
+
   return (
     <form
       className="field has-addons"
@@ -8,7 +14,12 @@ export const TodoFilter: React.FC = () => {
     >
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            onChange={e =>
+              dispatch(filterSlice.actions.setStatus(e.target.value as Status))
+            }
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -22,19 +33,25 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          value={query}
+          onChange={e => dispatch(filterSlice.actions.setQuery(e.target.value))}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {query.length > 0 && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              value={query}
+              onClick={() => dispatch(filterSlice.actions.setQuery(''))}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
---
+++ src/services/getPreparedTodos.ts
@@ -0,0 +1,34 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { FilterParams } from '../types/filterParams';
+import { Todo } from '../types/Todo';
+import { RootState } from '../app/store';
+
+export const selectPreparedTodos = createSelector(
+  [(state: RootState) => state.todos, (state: RootState) => state.filter],
+  (todos: Todo[], filterParams: FilterParams) => {
+    const { query, status } = filterParams;
+
+    let preparedTodos = [...todos];
+
+    switch (status) {
+      case 'active':
+        preparedTodos = preparedTodos.filter(todo => !todo.completed);
+        break;
+      case 'completed':
+        preparedTodos = preparedTodos.filter(todo => todo.completed);
+        break;
+      default:
+        break;
+    }
+
+    if (query) {
+      preparedTodos = preparedTodos.filter(todo =>
+        todo.title
+          .toLocaleLowerCase()
+          .includes(query.toLocaleLowerCase().trim()),
+      );
+    }
+
+    return preparedTodos;
+  },
+);
