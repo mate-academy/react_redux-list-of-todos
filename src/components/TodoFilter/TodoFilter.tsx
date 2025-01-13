83e5c2d661e8102c4f0295e81568ce3f01c5287e
Roman S <romanuch03@gmail.com>
2025-01-12 23:38:28 -0500

    Solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,39 +1,58 @@
-import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../../app/hooks';
+import { setQuery, setQueryClear, setStatus } from '../../features/filter';
 
-export const TodoFilter: React.FC = () => {
+export const TodoFilter = ({ }) => {
+  const dispatch = useDispatch();
+
+  const filters = useAppSelector(state => state.filters);
+
+  const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setStatus(event.target.value as 'all' | 'active' | 'completed'));
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setQuery(event.target.value));
+  };
+
   return (
-    <form
-      className="field has-addons"
-      onSubmit={event => event.preventDefault()}
-    >
+    <form className="field has-addons">
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            value={filters.status}
+            onChange={handleStatusChange}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
           </select>
         </span>
       </p>
-
       <p className="control is-expanded has-icons-left has-icons-right">
         <input
           data-cy="searchInput"
           type="text"
           className="input"
-          placeholder="Search..."
+          placeholder="Search todos..."
+          value={filters.query}
+          onChange={handleSearchChange}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
         <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
+
+          {filters.query && (
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => dispatch(setQueryClear())}
+            />
+          )}
         </span>
       </p>
     </form>
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,6 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
+type Filter = {
+  query: string;
+  status: 'all' | 'active' | 'completed';
+};
+
+const initialState: Filter = {
   query: '',
   status: 'all',
 };
@@ -8,5 +13,17 @@ const initialState = {
 export const filterSlice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+   reducers: {
+    setStatus(state, action: PayloadAction<'all' | 'active' | 'completed'>) {
+      return { ...state, status: action.payload };
+    },
+    setQuery(state, action: PayloadAction<string>) {
+      return { ...state, query: action.payload };
+    },
+    setQueryClear(state) {
+      return { ...state, query: '' };
+    },
+  },
 });
+
+export const { setStatus, setQuery, setQueryClear } = filterSlice.actions;
