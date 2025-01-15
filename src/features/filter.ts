a12c46d289ca5fcd09ec748b745355182976d58f
myemets <for.education1809@gmail.com>
2025-01-15 17:37:29 +0200

    solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Status } from '../types/Status';
 
 const initialState = {
   query: '',
-  status: 'all',
+  status: Status.All,
 };
 
 export const filterSlice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setQuery(filter, { payload }: PayloadAction<string>) {
+      filter.query = payload;
+    },
+    clearQuery(filter) {
+      filter.query = '';
+    },
+    setStatus(filter, { payload }: PayloadAction<Status>) {
+      filter.status = payload;
+    },
+  },
 });
