d7afe7771c1d79da961cd6c59133ce9d14d43dca
ogchy <ogchyx@gmail.com>
2025-02-05 21:52:56 +0200

    dev

--- src/features/todos.ts
+++ src/features/todos.ts
@@ -5,24 +5,12 @@ export const todosSlice = createSlice({
   name: 'todos',
   initialState: [] as Todo[],
   reducers: {
-    addTodo: (state, action: PayloadAction<Todo>) => {
-      state.push(action.payload);
-    },
-
-    removeTodo: (state, action: PayloadAction<Todo>) => {
-      return state.filter(todo => todo.id !== action.payload.id);
-    },
-
-    updateTodo: (state, action: PayloadAction<Todo>) => {
-      const index = state.findIndex(todo => todo.id === action.payload.id);
-
-      if (index !== -1) {
-        state[index] = action.payload;
-      }
+    todos: (_, action: PayloadAction<Todo[]>) => {
+      return action.payload;
     },
   },
 });
 
-export const { addTodo, removeTodo, updateTodo } = todosSlice.actions;
+export const { todos } = todosSlice.actions;
 
 export default todosSlice.reducer;
