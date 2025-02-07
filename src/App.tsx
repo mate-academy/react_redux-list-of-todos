8fa8ce1c5292db917559bc4440dcde1fb3b16f09
Dmytro Loboda <dmytro.loboda@gmail.com>
2025-02-07 17:59:07 +0800

    redux todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
+import { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { todosSlice } from './features/todos';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const dispatch = useAppDispatch();
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+  const [isLoading, setIsLoading] = useState(true);
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const loadTodos = async () => {
+    try {
+      const todos = await getTodos();
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+      dispatch(todosSlice.actions.loadTodos(todos));
+    } catch {
+      alert('Error to load todos');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadTodos();
+  });
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {isLoading ? (
+                <Loader />
+              ) : (
+                <TodoList selectedTodo={selectedTodo} />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <TodoModal />
-  </>
-);
+      {selectedTodo && <TodoModal selectedTodo={selectedTodo} />}
+    </>
+  );
+};
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,9 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { todosSlice } from '../features/todos';
+import { currentTodoSlice } from '../features/currentTodo';
+import { filterSlice } from '../features/filter';
 
-const rootReducer = combineSlices();
+const rootReducer = combineSlices(todosSlice, currentTodoSlice, filterSlice);
 
 export const store = configureStore({
   reducer: rootReducer,
