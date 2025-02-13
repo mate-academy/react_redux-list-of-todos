057dca794c5f03d4e1f19a752e083485579aecd3
AndreaBoiko <andriy.boyko35@gmail.com>
2025-02-13 17:47:42 +0100

    solution

--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const initialState = {
   query: '',
@@ -8,5 +8,15 @@ const initialState = {
 export const filterSlice = createSlice({
   name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setQuery: (state, { payload }: PayloadAction<string>) => ({
+      ...state,
+      query: payload,
+    }),
+    clearQuery: state => ({ ...state, query: '' }),
+    setStatus: (state, { payload }: PayloadAction<string>) => ({
+      ...state,
+      status: payload,
+    }),
+  },
 });
--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,6 +1,9 @@
+import { User } from './User';
+
 export interface Todo {
   id: number;
   title: string;
   completed: boolean;
   userId: number;
+  user?: User;
 }
