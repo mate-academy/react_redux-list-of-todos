c5f216057279269db5b1be127c5b0e343223aa1a
Artem Puzenko <a.puzenko2004@gmail.com>
2025-02-19 12:20:44 +0200

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getTodos } from './api';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { todosSlice } from './features/todos';
+import { Todo } from './types/Todo';
 
-export const App = () => {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const dispatch = useAppDispatch();
-  // const currentTodo = useAppSelector(state => state.currentTodo);
+export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useAppDispatch();
+  const currentTodo = useAppSelector(state => state.currentTodo);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const todosDb = await getTodos();
+
+      dispatch(todosSlice.actions.setTodos(todosDb as Todo[]));
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [dispatch]);
 
   return (
     <>
@@ -19,15 +35,11 @@ export const App = () => {
               <TodoFilter />
             </div>
 
-            <div className="block">
-              <Loader />
-              <TodoList />
-            </div>
+            <div className="block">{isLoading ? <Loader /> : <TodoList />}</div>
           </div>
         </div>
       </div>
-
-      <TodoModal />
+      {currentTodo && <TodoModal />}
     </>
   );
 };
